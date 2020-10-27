0c86615e36c6786324170bee6e44a56bb2154dbe
GABRYCA <gabrycaretti@gmail.com>
2020-10-27 17:45:13 +0100

    Changelogs: - Migliorato algoritmo numeroCasuale il giocoIndovinaNumero.exe

--- giocoIndovinaNumero/main.cpp
+++ giocoIndovinaNumero/main.cpp
@@ -17,14 +17,17 @@
 // Ogni volta se il numero inserito dall'utente è maggiore o minore di quello casuale, come consiglio.
 // Alla fine del programma si comunicheranno anche tutti i numeri inseriti e quello corretto casuale.
 
+void numeriProvati(int conta, const int *numeriInseriti);
+
 int main() {
 
     // Messaggi d'inizio
     printf("Hai scelto: Indovina numero casuale...\n");
-    printf("\nIl numero casuale da indovinare è compreso tra -10 e 20, hai 10 solo tentativi!");
+    printf("\nIl numero casuale da indovinare è compreso tra -10 e 20. Hai solo 10 tentativi!");
 
-    int tentativi = 10, conta = 1, x;
+    int tentativi = 10, conta = 0, x;
     int numeriInseriti[tentativi];
+
 
     // Ottengo x dall'utente
     printf("\nInserisci il numero con cui provare: ");
@@ -32,98 +35,69 @@ int main() {
 
     // Genero un numero casuale compreso tra 20 e -10
     srand(time(0));
-    int numeroCasuale = rand()%(20-(-10) + 1) + (-10);
+    int numeroCasuale = rand() % (20 - (-10) + 1) + (-10);
 
-    // Controllo se l'utente ha trovato il numero al primo tentativo
-    if (x == numeroCasuale){
+    // While che va avanti finchè i tentativi non sono uguali a 0
+    while (tentativi >= 0) {
 
-        printf("\nComplimenti, hai indovinato il numero al primo tentativo! Era %d", numeroCasuale);
+        // Assegna il valore al vettore e lo incrementa
+        numeriInseriti[conta++] = x;
 
-    // Vado avanti se non l'ha indovinato subito
-    } else {
+        // Se l'utente ha indovinato il numero, finisce
+        if (x == numeroCasuale) {
 
-        // Decremento il numero di tentativi rimasti
-        tentativi--;
+            // Messaggio della vittoria e quanti tentativi sono stati usati
+            printf("\nHai indovinato il numero al %d° tentativo", conta);
+            numeriProvati(conta, numeriInseriti);
 
-        // Comunico all'utente che il numero che aveva inserito era errato
-        printf("\nHai inserito un numero errato, hai ancora %d tentativi!\n", tentativi);
-        if (x > numeroCasuale){
+            // Ferma il programma
+            return 0;
+        } else {
 
-            // Consiglio per trovare il numero
-            printf("\nConsiglio: il numero inserito è -MAGGIORE- di quello da indovinare!");
-
-        } else if (x < numeroCasuale){
-
-            // Consiglio per trovare il numero
-            printf("\nConsiglio: Il numero inserito è -MINORE- di quello da indovinare!");
-
-        }
-
-        // While che va avanti finchè i tentativi non sono uguali a 0
-        while (tentativi >= 0){
-
-            // Assegna il valore al vettore e lo incrementa
-            numeriInseriti[conta++] = x;
+            // Decrementa il numero di tentativi rimasti
+            tentativi--;
 
             // L'utente ha finito i tentativi e ha quindi perso
-            if (tentativi <= 0){
+            if (tentativi <= 0) {
 
                 // Messaggio della sconfitta
                 printf("\nHai finito i tentativi! Hai inserito i seguenti numeri:");
+                numeriProvati(conta, numeriInseriti);
 
-                // Visualizzatore numeri inseriti
-                printf("\nI numeri che hai provato sono: \n");
-                for (int i = 1; i < conta; i++) {
-
-                    // Scrivo i numeri
-                    printf("%d \t", numeriInseriti[i]);
-
-                }
+                return 0;
             }
 
-            // Messaggio che invita l'utente a riprovare e quindi inserire un nuovo x
-            printf("\nInserisci il nuovo numero con cui provare: ");
-            scanf("%d", &x);
+            // Comunica all'utente che ha un numero errato
+            printf("\nHai inserito un numero errato, hai ancora %d tentativi!\n", tentativi);
+            if (x > numeroCasuale) {
 
-            // Se l'utente ha indovinato il numero, finisce
-            if (x == numeroCasuale){
+                // Consiglio
+                printf("\nConsiglio: il numero inserito è -MAGGIORE- di quello da indovinare!");
 
-                // Messaggio della vittoria e quanti tentativi sono stati usati
-                printf("\nHai indovinato il numero al %d° tentativo", conta);
+            } else if (x < numeroCasuale) {
 
-                // Visualizzatore numeri inseriti
-                printf("\nI numeri che hai provato sono: \n");
-                for (int i = 1; i < conta; i++) {
+                // Consiglio
+                printf("\nConsiglio: Il numero inserito è -MINORE- di quello da indovinare!");
 
-                    // Scrivo i numeri
-                    printf("%d \t", numeriInseriti[i]);
-
-                }
-
-                // Ferma il programma
-                return 0;
-            } else {
-
-                // Decrementa il numero di tentativi rimasti
-                tentativi--;
-
-                // Comunica all'utente che ha un numero errato
-                printf("\nHai inserito un numero errato, hai ancora %d tentativi!\n", tentativi);
-                if (x > numeroCasuale){
-
-                    // Consiglio
-                    printf("\nConsiglio: il numero inserito è -MAGGIORE- di quello da indovinare!");
-
-                } else if (x < numeroCasuale){
-
-                    // Consiglio
-                    printf("\nConsiglio: Il numero inserito è -MINORE- di quello da indovinare!");
-
-                }
             }
         }
-    }
 
+        // Messaggio che invita l'utente a riprovare e quindi inserire un nuovo x
+        printf("\nInserisci il nuovo numero con cui provare: ");
+        scanf("%d", &x);
+
+    }
     return 0;
+}
+
+void numeriProvati(int conta, const int *numeriInseriti) {// Visualizzatore numeri inseriti
+
+    printf("\nI numeri che hai provato sono: \n");
+    for (int i = 0; i < conta; i++) {
+
+        // Scrivo i numeri
+        printf("%d \t", numeriInseriti[i]);
+
+    }
 }
 
