7f81ca70a32e66deb04dcd169ba40cbe46663373
Alex936-CTRL <alexcst2004@gmail.com>
2020-11-02 16:47:36 +0100

    Aggiunti algoritmi con l'utilizzo del rand e funzioni logaritmica e delle potenze

--- Menu vari/menuVettori/cmake-build-debug/cmake_install.cmake
+++ Menu vari/menuVettori/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /cygdrive/c/Users/GABRYCA/CLionProjects/Algoritmi_scuola/Menu vari/menuVettori
+# Install script for directory: /cygdrive/c/Users/alexc/CLionProjects/Algoritmi_scuola/Menu vari/menuVettori
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/cygdrive/c/Users/GABRYCA/CLionProjects/Algoritmi_scuola/Menu vari/menuVettori/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/cygdrive/c/Users/alexc/CLionProjects/Algoritmi_scuola/Menu vari/menuVettori/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
