97a92685eab4b6cf10e44646847499572ce4496b
giuse84 <56880367+giuse84@users.noreply.github.com>
2020-11-16 23:10:32 +0100

    modifiche notturne

---
+++ Menu vari/Giuse/menu vettori/cmake-build-debug/CMakeFiles/menu2_array.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/menu2_array.dir/main.cpp.o"
+  "menu2_array"
+  "menu2_array.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/menu2_array.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
