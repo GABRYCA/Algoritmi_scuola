ab806013248e80a0e65005ea806197197a14d5ca
GABRYCA <gabrycaretti@gmail.com>
2021-01-25 18:54:46 +0100

    Aggiunto a stringa:
    - Lunghezza Menu.
    - Unisci.

--- C++/Stringa/cmake-build-debug/cmake_install.cmake
+++ C++/Stringa/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /cygdrive/c/Users/GABRYCA/CLionProjects/Algoritmi_scuola/C++/Stringhe
+# Install script for directory: /cygdrive/c/Users/GABRYCA/CLionProjects/Algoritmi_scuola/C++/Stringa
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/cygdrive/c/Users/GABRYCA/CLionProjects/Algoritmi_scuola/C++/Stringhe/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/cygdrive/c/Users/GABRYCA/CLionProjects/Algoritmi_scuola/C++/Stringa/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- C++/Stringa/main.cpp
+++ C++/Stringa/main.cpp
@@ -5,6 +5,10 @@ void continua();
 
 bool compare(int lunghezzaMassima, char stringa[], char stringa2[]);
 
+int dimensioni(int lunghezzaMassima, char stringa[]);
+
+void unisci(int lunghezzaMassima, char stringa[], char stringa2[]);
+
 int main() {
 
     // Messaggio del creatore
@@ -14,8 +18,8 @@ int main() {
 
     int scelta = 1;
     int lunghezzaMassima = 100;
-    char stringa[100] = {"n"};
-    char stringa2[100] = {"n"};
+    char stringa[lunghezzaMassima];
+    char stringa2[lunghezzaMassima];
 
     while (scelta != 0){
 
@@ -26,6 +30,8 @@ int main() {
                "\n 3 -> Confronta stringa 1 a 2."
                "\n 4 -> Mostra stringa 1."
                "\n 5 -> Mostra stringa 2."
+               "\n 6 -> Lunghezza stringa."
+               "\n 7 -> Unisci stringa 1 e 2 in Stringa 1."
                "\nScelta: ");
         scanf("%d", &scelta);
 
@@ -44,7 +50,7 @@ int main() {
                 printf("\nHai scelto: Inserisci stringa 1...");
 
                 printf("\nInserire stringa: ");
-                scanf(" %s", stringa);
+                scanf("%s", stringa);
 
                 printf("\nStringa 1 inserita con successo!");
 
@@ -57,7 +63,7 @@ int main() {
                 printf("\n\nHai scelto: Inserisci stringa 2...");
 
                 printf("\nInserire stringa: ");
-                scanf(" %s", stringa2);
+                scanf("%s", stringa2);
 
                 printf("\nStringa 2 inserita con successo!");
 
@@ -99,6 +105,75 @@ int main() {
                 break;
             }
 
+            case 6:{
+
+                printf("\nHai scelto: Lunghezza stringa...");
+
+                int nStringa = 1;
+
+                while (nStringa != 0) {
+                    printf("\nScegli una delle stringhe: "
+                           "\n 0 -> Esci."
+                           "\n 1 -> Stringa 1."
+                           "\n 2 -> Stringa 2."
+                           "\nScelta: ");
+                    scanf("%d", &nStringa);
+
+                    switch (nStringa) {
+
+                        case 0:{
+
+                            printf("\nHai scelto: Esci...");
+
+                            break;
+                        }
+
+                        case 1:{
+
+                            printf("\nHai scelto: Stringa 1");
+
+                            printf("\nLa stringa 1 ha dimensioni: %d.", dimensioni(lunghezzaMassima, stringa));
+
+                            continua();
+                            break;
+                        }
+
+                        case 2:{
+
+                            printf("\nHai scelto: Stringa 2");
+
+                            printf("\nLa stringa 2 ha dimensioni: %d", dimensioni(lunghezzaMassima, stringa2));
+
+                            continua();
+                            break;
+                        }
+
+                        default:{
+
+                            printf("\nHai inserito una scelta non valida!");
+
+                            continua();
+                            break;
+                        }
+                    }
+                }
+
+                continua();
+                break;
+            }
+
+            case 7:{
+
+                printf("\nHai scelto: Unisci stringhe 1 e 2 in Stringa 1...");
+
+                unisci(lunghezzaMassima, stringa, stringa2);
+
+                printf("\nUniti con successo stringa 1 e 2 in Stringa 1 -> %s", stringa);
+
+                continua();
+                break;
+            }
+
             default:{
 
                 printf("\nHai inserito una scelta non valida.");
@@ -112,6 +187,33 @@ int main() {
     printf("\nEsercizio chiuso con successo!");
 
     return 0;
+}
+
+void unisci(int lunghezzaMassima, char stringa[], char stringa2[]) {
+
+    int lungStr1 = dimensioni(lunghezzaMassima, stringa);
+    int lungStr2 = dimensioni(lunghezzaMassima, stringa2);
+
+    for (int i = 0; i < lungStr2; i++) {
+        stringa[lungStr1 + i] = stringa2[i];
+    }
+
+}
+
+int dimensioni(int lunghezzaMassima, char stringa[]) {
+
+    int dimensioni = 0;
+    char nullo[2] = "\0";
+
+    for (int i = 0; i < lunghezzaMassima; i++) {
+        if (stringa[i] == nullo[1]){
+            return dimensioni;
+        } else {
+            dimensioni = i + 1;
+        }
+    }
+
+    return dimensioni;
 }
 
 // Ritorna true se sono uguali.
