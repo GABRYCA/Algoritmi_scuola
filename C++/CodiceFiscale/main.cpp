a465bb39cd9247456591ef198e359c6b99bf224d
GABRYCA <gabrycaretti@gmail.com>
2021-02-05 10:30:03 +0100

    Finito e commenti.

--- C++/CodiceFiscale/main.cpp
+++ C++/CodiceFiscale/main.cpp
@@ -38,16 +38,19 @@ int main() {
 
             case 1:{
 
+                // Data
                 struct s_data{
                     string anno, giorno;
                     int mese = 0;
                 };
 
+                // Persona
                 struct s_persona{
                     string nome, cognome, comune, sesso;
                     s_data dataNascita;
                 } persona;
 
+                // Inserimento dati:
                 printf("\nHai scelto: Codice fiscale.");
 
                 printf("\nInserire sesso (M Maschio, F Femmina): ");
@@ -71,10 +74,13 @@ int main() {
                 printf("\nInserire anno di nascita AAAA: ");
                 cin >> persona.dataNascita.anno;
 
+                // Dichiaro stringa codice fiscale.
                 string codiceFiscale;
 
+                // Comunico inizio conversione.
                 printf("\nGenerazione codice fiscale in corso...");
 
+                // Dichiaro variabile utile e carattere speciale
                 int numerLet = 0;
                 string carattereSpeciale = "X";
 
@@ -150,7 +156,6 @@ int main() {
 
                 // Inizio 2 numeri per anno di nascita.
                 codiceFiscale += persona.dataNascita.anno.substr(2);
-                numerLet += 2;
                 // Fine 2 numeri per anno di nascita.
 
                 // Inizio lettera per mese di nascita.
@@ -247,10 +252,10 @@ int main() {
                     }
 
                 }
-                numerLet++;
                 // Fine lettere per mese di nascita.
 
                 // Inizio numeri per giorno di nascita.
+
                 int giornoNascita = atoi(persona.dataNascita.giorno.c_str());
                 stringstream ss1;
                 stringstream ss2;
@@ -264,11 +269,394 @@ int main() {
                 } else {
                     codiceFiscale += ss2.str();
                 }
-                numerLet += 2;
                 // Fine numeri per giorno di nascita.
 
+                // Inserimento codice comune:
+
+                string codiceComune;
+                printf("\n\nInserire codice comune (1 lettera e 3 numeri, es H501): ");
+                cin >> codiceComune;
+
+                // Aggiungo al codice fiscale il codice del comune inserito dall'utente.
+                codiceFiscale += codiceComune;
+
+                printf("\n\nRipresa generazione codice fiscale...");
+
+                // Generazione carattere di controllo.
+
+                // Rende tutto uppercase.
                 std::transform(codiceFiscale.begin(), codiceFiscale.end(),codiceFiscale.begin(), ::toupper);
 
+                // Variabile somma dei valori e alfabeto.
+                int sommaValori = 0;
+                string alfabeto = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
+
+                // Ciclo per ogni carattere del codice fiscale
+                for (int i = 0; i < codiceFiscale.size(); i++) {
+                    // Ottengo posizione corretta.
+                    int posizione = i + 1;
+
+                    // Verifica se è pari o dispari
+                    if (posizione % 2 == 0){
+
+                        // Esegui azione in base al valore.
+                        switch (codiceFiscale[i]) {
+
+                            case '0':{
+
+                                sommaValori += 0;
+
+                                break;
+                            }
+
+                            case '1':{
+
+                                sommaValori += 1;
+
+                                break;
+                            }
+
+                            case '2':{
+
+                                sommaValori += 2;
+
+                                break;
+                            }
+
+                            case '3':{
+
+                                sommaValori += 3;
+
+                                break;
+                            }
+
+                            case '4':{
+
+                                sommaValori += 4;
+
+                                break;
+                            }
+
+                            case '5':{
+
+                                sommaValori += 5;
+
+                                break;
+                            }
+
+                            case '6':{
+
+                                sommaValori += 6;
+
+                                break;
+                            }
+
+                            case '7':{
+
+                                sommaValori += 7;
+
+                                break;
+                            }
+
+                            case '8':{
+
+                                sommaValori += 8;
+
+                                break;
+                            }
+
+                            case '9':{
+
+                                sommaValori += 9;
+
+                                break;
+                            }
+
+                            default:{
+
+                                // Ciclo logico in caso non sia numero ma una lettera.
+                                for (int j = 0; j < alfabeto.size(); j++) {
+                                    if (codiceFiscale[i] == alfabeto[j]){
+                                        sommaValori += j;
+                                    }
+                                }
+
+                                break;
+                            }
+                        }
+
+                    } else {
+
+                        // Eseguo azioni in base al valore.
+                        switch (codiceFiscale[i]) {
+                            case 'A':{
+
+                                sommaValori += 1;
+
+                                break;
+                            }
+
+                            case 'B':{
+
+                                sommaValori += 0;
+
+                                break;
+                            }
+
+                            case 'C':{
+
+                                sommaValori += 5;
+
+                                break;
+                            }
+
+                            case 'D':{
+
+                                sommaValori += 7;
+
+                                break;
+                            }
+
+                            case 'E':{
+
+                                sommaValori += 9;
+
+                                break;
+                            }
+
+                            case 'F':{
+
+                                sommaValori += 13;
+
+                                break;
+                            }
+
+                            case 'G':{
+
+                                sommaValori += 15;
+
+                                break;
+                            }
+
+                            case 'H':{
+
+                                sommaValori += 17;
+
+                                break;
+                            }
+
+                            case 'I':{
+
+                                sommaValori += 19;
+
+                                break;
+                            }
+
+                            case 'J':{
+
+                                sommaValori += 21;
+
+                                break;
+                            }
+
+                            case 'K':{
+
+                                sommaValori += 2;
+
+                                break;
+                            }
+
+                            case 'L':{
+
+                                sommaValori += 4;
+
+                                break;
+                            }
+
+                            case 'M':{
+
+                                sommaValori += 18;
+
+                                break;
+                            }
+
+                            case 'N':{
+
+                                sommaValori += 20;
+
+                                break;
+                            }
+
+                            case 'O':{
+
+                                sommaValori += 11;
+
+                                break;
+                            }
+
+                            case 'P':{
+
+                                sommaValori += 3;
+
+                                break;
+                            }
+
+                            case 'Q':{
+
+                                sommaValori += 6;
+
+                                break;
+                            }
+
+                            case 'R':{
+
+                                sommaValori += 8;
+
+                                break;
+                            }
+
+                            case 'S':{
+
+                                sommaValori += 12;
+
+                                break;
+                            }
+
+                            case 'T':{
+
+                                sommaValori += 14;
+
+                                break;
+                            }
+
+                            case 'U':{
+
+                                sommaValori += 16;
+
+                                break;
+                            }
+
+                            case 'V':{
+
+                                sommaValori += 10;
+
+                                break;
+                            }
+
+                            case 'W':{
+
+                                sommaValori += 22;
+
+                                break;
+                            }
+
+                            case 'X':{
+
+                                sommaValori += 25;
+
+                                break;
+                            }
+
+                            case 'Y':{
+
+                                sommaValori += 24;
+
+                                break;
+                            }
+
+                            case 'Z':{
+
+                                sommaValori += 23;
+
+                                break;
+                            }
+
+                            case '0':{
+
+                                sommaValori += 1;
+
+                                break;
+                            }
+
+                            case '1':{
+
+                                sommaValori += 0;
+
+                                break;
+                            }
+
+                            case '2':{
+
+                                sommaValori += 5;
+
+                                break;
+                            }
+
+                            case '3':{
+
+                                sommaValori += 7;
+
+                                break;
+                            }
+
+                            case '4':{
+
+                                sommaValori += 9;
+
+                                break;
+                            }
+
+                            case '5':{
+
+                                sommaValori += 13;
+
+                                break;
+                            }
+
+                            case '6':{
+
+                                sommaValori += 15;
+
+                                break;
+                            }
+
+                            case '7':{
+
+                                sommaValori += 17;
+
+                                break;
+                            }
+
+                            case '8':{
+
+                                sommaValori += 19;
+
+                                break;
+                            }
+
+                            case '9':{
+
+                                sommaValori += 21;
+
+                                break;
+                            }
+
+                            default:{
+
+                                sommaValori += 0;
+
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                // Ottengo lettera in base al valore del resto.
+                codiceFiscale += alfabeto[(sommaValori % 26)];
+
+                // Fine carattere di controllo.
+
+                // Comunico.
                 cout << "\nCodice fiscale: " << codiceFiscale;
 
                 continua();
@@ -294,8 +682,10 @@ int main() {
 
 bool carattereEssereConsonante(int i, string nome) {
 
+    // Insieme vocali.
     string vocali = "aAeEiIoOuU";
 
+    // Ciclo logico
     for (int j = 0; j < vocali.size(); j++) {
         if (nome[i] == vocali[j]){
             return false;
