7fd0ec80ed24c5da70d85bfd9c14d09f29491b21
GABRYCA <gabrycaretti@gmail.com>
2021-03-26 10:23:15 +0100

    Nuovo esercizio FILE e abuso di variabili long innocenti.

--- C++/OrdinamentoValVettore/main.cpp
+++ C++/OrdinamentoValVettore/main.cpp
@@ -7,7 +7,9 @@ using namespace std;
 
 void continua();
 
-long random(int min, int max);
+long random(long min, long max);
+
+void genSuFileValCasuali(long nNumeri, long max, long min, FILE *cfFile);
 
 int main() {
 
@@ -23,6 +25,7 @@ int main() {
                "\n0 -> Esci."
                "\n1 -> Ordinamento vettore numeri casuali."
                "\n2 -> Ordinamento vettore numeri casuali 2."
+               "\n3 -> Ordinamento FILE inefficiente senza vettori."
                "\nScelta: ");
         scanf("%d", &scelta);
 
@@ -41,13 +44,13 @@ int main() {
 
                 // Inserimento dati per variabili dell'utente.
                 long nNumeri;
-                int max, min;
+                long max, min;
                 printf("\nInserire numero di valori da generare: ");
                 scanf("%ld", &nNumeri);
                 printf("\nInserire numero massimo possibile: ");
-                scanf("%d", &max);
+                scanf("%ld", &max);
                 printf("\nInserire numero minore: ");
-                scanf("%d", &min);
+                scanf("%ld", &min);
 
                 srand(time(0));
 
@@ -109,13 +112,13 @@ int main() {
 
                 // Inserimento dati per variabili dell'utente.
                 long nNumeri;
-                int max, min;
+                long max, min;
                 printf("\nInserire numero di valori da generare: ");
                 scanf("%ld", &nNumeri);
                 printf("\nInserire numero massimo possibile: ");
-                scanf("%d", &max);
+                scanf("%ld", &max);
                 printf("\nInserire numero minore: ");
-                scanf("%d", &min);
+                scanf("%ld", &min);
 
                 srand(time(0));
 
@@ -135,10 +138,10 @@ int main() {
                 inizio = clock();
 
                 long vettoreNuovo[nNumeri];
-                int contatore = 0;
-                for(int i = min; i <= max; i++){
+                long contatore = 0;
+                for(long i = min; i <= max; i++){
 
-                    for(int j = 0; j < nNumeri; j++){
+                    for(long j = 0; j < nNumeri; j++){
 
                         if(vettoreConNumeri[j] == i){
 
@@ -149,7 +152,7 @@ int main() {
                 }
 
                 // Ricopio i numeri del vettore di appoggio nell'originale
-                for (int i = 0; i < nNumeri; i++) {
+                for (long i = 0; i < nNumeri; i++) {
                     vettoreConNumeri[i] = vettoreNuovo[i];
                 }
 
@@ -171,6 +174,94 @@ int main() {
                 break;
             }
 
+            case 3:{
+
+                printf("\nHai scelto: Riordinamento FILE inefficiente.");
+
+                // Inserimento dati per variabili dell'utente.
+                long nNumeri;
+                long max, min;
+                printf("\nInserire numero di valori da generare: ");
+                scanf("%ld", &nNumeri);
+                printf("\nInserire numero massimo possibile: ");
+                scanf("%ld", &max);
+                printf("\nInserire numero minore: ");
+                scanf("%ld", &min);
+
+                srand(time(0));
+
+                // Genero valori casuali tra max e min per tutto il FILE scrivendoli.
+                FILE *cfFile;
+                cfFile = fopen("file.txt", "w");
+                clock_t inizio = clock();
+                genSuFileValCasuali(nNumeri, max, min, cfFile);
+                clock_t fine = clock();
+                fclose(cfFile);
+
+                // Tempo necessario alla generazione.
+                unsigned long tempoGenerazione = (fine - inizio)/CLOCKS_PER_SEC;
+
+                // Scrittura.
+                FILE *cfTemp;
+                cfTemp = fopen("fileTemp.txt", "w");
+
+                inizio = clock();
+
+                // Riordinamento.
+                long contatore = 0;
+                for(long i = min; i <= max; i++){
+
+                    // Letttura.
+                    cfFile = fopen("file.txt", "r");
+                    while (!feof(cfFile)){
+
+                        long numero;
+                        fscanf(cfFile, "%ld", &numero);
+                        if(numero == i){
+                            if (contatore != 0){
+                                fprintf(cfTemp, "%s", "\n");
+                            }
+                            fprintf(cfTemp, "%ld", numero);
+                            contatore++;
+                        }
+                    }
+                    fclose(cfFile);
+                }
+                fclose(cfTemp);
+
+                fine = clock();
+
+                // Tempo necessario al riordinamento.
+                unsigned long tempoRiordinamento = (fine - inizio)/CLOCKS_PER_SEC;
+
+                // Cancella vecchio FILE e rinomina quello nuovo per sostituirlo.
+                remove("file.txt");
+                rename("fileTemp.txt", "file.txt");
+
+                printf("\nOrdinamento su FILE temporaneo effettuato con successo!");
+
+                // Lettura FILE finale.
+                cfFile = fopen("file.txt", "r");
+                if (cfFile == NULL){
+                    printf("\nErrore durante la lettura del FILE.");
+                } else {
+                    printf("\nLettura FILE ordinato:");
+                    while (!feof(cfFile)){
+                        long numero;
+                        fscanf(cfFile, "%ld", &numero);
+                        printf("\n%ld", numero);
+                    }
+                    fclose(cfFile);
+                }
+
+                printf("\n\nTempo necessario per generare e scrivere numeri su FILE: %ld secondi.", tempoGenerazione);
+                printf("\nTempo necessario per riordinare i numeri su FILE: %ld secondi.", tempoRiordinamento);
+
+                printf("\nFine programma...");
+                continua();
+                break;
+            }
+
             default:{
 
                 printf("\nScelta non valida, per favore riprovare!");
@@ -184,6 +275,21 @@ int main() {
     return 0;
 }
 
+void genSuFileValCasuali(long nNumeri, long max, long min, FILE *cfFile) {
+    if (cfFile == NULL){
+        printf("\nErrore durante la creazione del file!");
+    } else {
+        for (long i = 0; i < nNumeri; i++) {
+            if (i != 0){
+                fprintf(cfFile, "%s", "\n");
+            }
+            long numeroCasuale = random(min, max);
+            fprintf(cfFile, "%ld", numeroCasuale);
+        }
+        printf("\nCreazione FILE Con valori casuali effettuata con successo!");
+    }
+}
+
 void continua(){
 
     // Dichiaro una variabile bandiera
@@ -195,7 +301,7 @@ void continua(){
 
 }
 
-long random(int min, int max){
-    int randomMN1 = max - min + 1 ;
-    return ((int) rand() % randomMN1 + min);
+long random(long min, long max){
+    long randomMN1 = max - min + 1 ;
+    return ((long) rand() % randomMN1 + min);
 }
