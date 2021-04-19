0aeaa28c11f12915216ce2430d3782b3d263a9ee
GABRYCA <gabrycaretti@gmail.com>
2021-04-19 12:13:31 +0200

    Commenti e cleanup.

--- C++/Records/main.cpp
+++ C++/Records/main.cpp
@@ -21,14 +21,18 @@ int main() {
            "\n//           Records di Gabriele Caretti 3BITI              //"
            "\n//////////////////////////////////////////////////////////////\n");
 
+    // Dichiaro struct.
     struct clientData blankData = {0, "", "", 0.0};
     struct clientData client{};
 
+    // Inizializzo puntatore FILE e variabile scelte.
     int scelta = 1;
     FILE *cfPtr;
 
+    // Ciclo che continua fino ad input = 0.
     while (scelta != 0){
 
+        // Legenda.
         printf("\nScegli cosa fare: "
                "\n0 -> Esci."
                "\n1 -> Inizializza Records."
@@ -40,10 +44,12 @@ int main() {
 
         // fopen wb inizializza, wb+ ci scrive anche se inizializzato in precedenza.
 
+        // Switch tra le scelte.
         switch (scelta) {
 
             case 0:{
 
+                // Messaggio opzione uscita scelta.
                 printf("\nHai scelto: Esci..."
                        "\n\nUscita in corso...");
 
@@ -52,18 +58,23 @@ int main() {
 
             case 1:{
 
+                // Messaggio d'inizio.
                 printf("\nHai scelto: Inizializza File records...");
 
+                // Apro il FILE nuovo.
                 cfPtr = fopen("clienti.txt", "wb");
 
+                // Inizializzo i valori nel FILE.
                 for (int i = 0; i < 100; i++) {
                     fseek(cfPtr, i * sizeof(struct clientData), SEEK_SET);
                     fwrite(&blankData, sizeof(struct clientData), 1, cfPtr);
                 }
 
+                // Chiudo il FILE e comunico FINE operazione.
                 fclose(cfPtr);
                 printf("\n\nInizializzato con successo.");
 
+                // Pausa.
                 continua();
                 break;
             }
@@ -94,100 +105,70 @@ int main() {
                 if (numeroRecord != 0){
 
                     // Apro il file e uno temporaneo.
-                    FILE *cfTempR = fopen("clienti.txt", "rb+");
-                    // cfPtr = fopen("clienti2.txt", "wb+");
+                    cfPtr = fopen("clienti.txt", "rb+");
 
+                    // Verifico che il FILE esista.
+                    if (cfPtr != NULL){
 
+                        // Scrivo il valore.
+                        fseek(cfPtr, (numeroRecord - 1) * sizeof(struct clientData), SEEK_SET);
+                        fwrite(&client, sizeof(struct clientData), 1, cfPtr);
 
-                    // Vecchio codice.
-                    // Dichiaro una struct di lettura per il file originale per poi richiarla in quello nuovo.
-                    /*struct clientData temporaneo{};
-
-                    if (cfTempR != NULL && cfPtr != NULL) {
-
-                    // Per ogni possibile posizione faccio un ciclo.
-                    for (int i = 0; i < 100; i++) {
-
-                    // Se la posizione non è quella da modificare, allora copio i valori.
-                    if (i != numeroRecord - 1) {
-
-                    // Imposto posizioni in cui leggere o scrivere per la copia.
-                    fseek(cfTempR, i * sizeof(struct clientData), SEEK_SET);
-                    fseek(cfPtr, i * sizeof(struct clientData), SEEK_SET);
-
-                    // Leggo dal file iniziali il valore e lo salvo nel file nuovo.
-                    fread(&temporaneo, sizeof(struct clientData), 1, cfTempR);
-                    fwrite(&temporaneo, sizeof(struct clientData), 1, cfPtr);
-
-                    // Se è quella da modificare, scrivo il nuovo valore.
-                    } else {
-
-                    // Scrivo nel file temporaneo il valore da aggiungere o modificare alla posizione data.
-                    fseek(cfPtr, (numeroRecord - 1) * sizeof(struct clientData), SEEK_SET);
-                    fwrite(&client, sizeof(struct clientData), 1, cfPtr);
-
-                    }
-                    }
-                    }
-
-                    // Chiudo i FILEs.
-                    fclose(cfPtr);
-                    fclose(cfTempR);
-
-                    // Cancello il vecchio FILE e rinomino quello temporaneo per sostituirlo.
-                    remove("clienti.txt");
-                    rename("clienti2.txt", "clienti.txt");*/
-
-                    if (cfTempR != NULL){
-
-                        fseek(cfTempR, (numeroRecord - 1) * sizeof(struct clientData), SEEK_SET);
-                        fwrite(&client, sizeof(struct clientData), 1, cfTempR);
+                        // Messaggio di successo.
+                        printf("\nSalvato con successo!");
 
                     } else {
 
+                        // Errore.
                         printf("\nErrore: FILE inesistente o non trovato!");
 
                     }
 
-                    fclose(cfTempR);
-
-                    printf("\nSalvato con successo!");
+                    // Chiudo il FILE.
+                    fclose(cfPtr);
 
                 } else {
 
+                    // Errore di posizione.
                     printf("\nHai inserito una posizione non valida!");
 
                 }
 
+                // Fine operazione.
                 printf("\n\nOperazione conclusa.");
 
-
+                // Pausa.
                 continua();
                 break;
             }
 
             case 3:{
 
+                // Messaggio d'inizio.
                 printf("\nHai scelto: Lettura Record...");
 
+                // Variabile e richiesta Input utente.
                 int numeroLeggere;
-
                 printf("\n\nInserire il numero della struct da leggere da 1 a 100: ");
                 cin >> numeroLeggere;
 
+                // Verifica che sia diverso da 0 la posizione.
                 if (numeroLeggere != 0){
 
-                    cfPtr = fopen("clienti.txt", "rb+");
+                    // Apro il FILE.
+                    cfPtr = fopen("clienti.txt", "rb");
 
+                    // Verifico che esista e faccio operazioni.
                     if(cfPtr != NULL){
 
+                        // Leggo il valore.
                         fseek(cfPtr, (numeroLeggere - 1) * sizeof(struct clientData), SEEK_SET);
-
                         fread(&client, sizeof(struct clientData), 1, cfPtr);
 
                         //fscanf(cfPtr, "%d%s%s%lf", &client.numeroConto, client.nomeProprietario, client.nomeEsecutore, &client.valore);
                         fclose(cfPtr);
 
+                        // Comunico dati letti.
                         printf("\nDati letti:"
                                "\nNumero: %d"
                                "\nProprietario %s"
@@ -196,6 +177,7 @@ int main() {
 
                     } else {
 
+                        // Errore.
                         printf("\nImpossibile aprire il FILE, forse non esiste.");
 
                     }
@@ -203,81 +185,73 @@ int main() {
 
                 } else {
 
+                    // Posizione scelta non valida.
                     printf("\nHai inserito un numero di posizione non valido!");
 
                 }
 
 
+                // Messaggio fine operazione.
                 printf("\nOperazione terminata..");
 
+
+                // PAUSA.
                 continua();
                 break;
             }
 
             case 4:{
 
+                // Messaggio d'inizio.
                 printf("\nHai scelto: Cancella qualcosa...");
 
+                // Variabile posizione e richiesta input utente.
                 int posizione;
                 printf("\n\nInserire posizione dato da cancellare: ");
                 scanf("%d", &posizione);
 
+                // Verifico che la posizione sia valida.
                 if (posizione != 0) {
 
-                    FILE *cfTempR = fopen("clienti.txt", "rb+");
-                    cfPtr = fopen("clienti2.txt", "wb+");
+                    // Apro il FILE.
+                    cfPtr = fopen("clienti.txt", "rb+");
 
-                    struct clientData temporaneo{};
+                    // Verifico che il FILE esista.
+                    if (cfPtr != NULL){
 
-                    if (cfTempR != NULL && cfPtr != NULL) {
+                        // Scrivo il valore.
+                        fseek(cfPtr, (posizione - 1) * sizeof(struct clientData), SEEK_SET);
+                        fwrite(&blankData, sizeof(struct clientData), 1, cfPtr);
 
-                        // Per ogni possibile posizione faccio un ciclo.
-                        for (int i = 0; i < 100; i++) {
+                        // Messaggio di successo.
+                        printf("\nSalvato con successo!");
 
-                            // Se la posizione non è quella da modificare, allora copio i valori.
-                            if (i != posizione - 1) {
+                    } else {
 
-                                // Imposto posizioni in cui leggere o scrivere per la copia.
-                                fseek(cfTempR, i * sizeof(struct clientData), SEEK_SET);
-                                fseek(cfPtr, i * sizeof(struct clientData), SEEK_SET);
+                        // Errore.
+                        printf("\nErrore: FILE inesistente o non trovato!");
 
-                                // Leggo dal file iniziali il valore e lo salvo nel file nuovo.
-                                fread(&temporaneo, sizeof(struct clientData), 1, cfTempR);
-                                fwrite(&temporaneo, sizeof(struct clientData), 1, cfPtr);
-
-                                // Se è quella da modificare, scrivo il nuovo valore.
-                            } else {
-
-                                // Scrivo nel file temporaneo il valore da aggiungere o modificare alla posizione data.
-                                fseek(cfPtr, (posizione - 1) * sizeof(struct clientData), SEEK_SET);
-                                fwrite(&blankData, sizeof(struct clientData), 1, cfPtr);
-
-                            }
-                        }
                     }
 
-                    // Chiudo i FILEs.
-                    fclose(cfPtr);
-                    fclose(cfTempR);
-
-                    remove("clienti.txt");
-                    rename("clienti2.txt", "clienti.txt");
-
+                    // Comunico messaggio di successo e chiudo il FILE.
                     printf("\nDato elimato con successo alla posizione %d.", posizione);
+                    fclose(cfPtr);
 
                 } else {
 
+                    // Comunico errore posizione.
                     printf("\nPosizione non valida.");
 
                 }
 
-
+                // Chiedo se continuare, pausa.
                 continua();
                 break;
             }
 
             default:{
 
+                // Comunico scelta non valida.
                 printf("\n\nHai inserito un valore non valido, riprovare!");
 
                 break;
@@ -286,9 +260,10 @@ int main() {
 
     }
 
+    // Messaggio di chiusura con successo.
     printf("\n\nUscito con successo!");
 
-
+    // FINE.
     return 0;
 }
 
