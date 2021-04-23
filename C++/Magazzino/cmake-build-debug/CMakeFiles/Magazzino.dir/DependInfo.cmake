986d1f08ac9256b5888818776cb2ea523161b627
GABRYCA <gabrycaretti@gmail.com>
2021-04-23 13:39:34 +0200

    Inizio magazzino, ancora da finire opzione 3 e 8 + test pesante.

--- C++/Magazzino/cmake-build-debug/CMakeFiles/Magazzino.dir/DependInfo.cmake
+++ C++/Magazzino/cmake-build-debug/CMakeFiles/Magazzino.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/cygdrive/c/Users/gabry/CLionProjects/Magazzino/main.cpp" "/cygdrive/c/Users/gabry/CLionProjects/Magazzino/cmake-build-debug/CMakeFiles/Magazzino.dir/main.cpp.o"
+  "/cygdrive/c/Users/gabry/CLionProjects/Algoritmi_scuola/C++/Magazzino/main.cpp" "/cygdrive/c/Users/gabry/CLionProjects/Algoritmi_scuola/C++/Magazzino/cmake-build-debug/CMakeFiles/Magazzino.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- C++/Magazzino/cmake-build-debug/cmake_install.cmake
+++ C++/Magazzino/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /cygdrive/c/Users/gabry/CLionProjects/Magazzino
+# Install script for directory: /cygdrive/c/Users/gabry/CLionProjects/Algoritmi_scuola/C++/Magazzino
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/cygdrive/c/Users/gabry/CLionProjects/Magazzino/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/cygdrive/c/Users/gabry/CLionProjects/Algoritmi_scuola/C++/Magazzino/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
