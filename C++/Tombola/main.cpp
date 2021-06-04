9f046c29306f0528a859d80f0ce81744e247c6a5
GABRYCA <gabrycaretti@gmail.com>
2021-06-04 20:45:06 +0200

    Inizio statistiche. Mancano statistiche ed eventuali opzioni extra per simulare le partite, mancano anche FILE su cui salvare i dati delle singole partite statistiche e poi leggerli per dare i risultati finali.

--- C++/Tombola/main.cpp
+++ C++/Tombola/main.cpp
@@ -18,7 +18,7 @@ int randomMaxMin(int numeroMin, int numeroMax);
 
 void mostraCartella(const cartella *cartelle, int generateSuccesso);
 
-void generatoreCartelle(int numeroGiocatori, cartella *cartelle);
+void generatoreCartelle(int numeroGiocatori, cartella *cartelle, bool puoScegliere);
 
 bool controlloNumeroCG(cartella *cartelle, int numeroGenerato, int i);
 
@@ -53,7 +53,7 @@ int main() {
 
         printf("\nLegenda:"
                "\n0 -> Esci."
-               "\n1 -> Inizio gioco (Conferma ad ognu turno)."
+               "\n1 -> Inizio gioco (Conferma ad ogni turno)."
                "\n2 -> Inizio gioco veloce (Mostra solamente un riepilogo)."
                "\nScelta: ");
         scanf("%d", &scelta);
@@ -72,12 +72,11 @@ int main() {
 
                 printf("\nHai scelto: Inizio gioco normale...");
 
-                bool pausaTurni = true;
                 int numeroGiocatori;
                 printf("\n\nInserire numero giocatori: ");
                 scanf("%d", &numeroGiocatori);
 
-                giocoTombola(pausaTurni, numeroGiocatori);
+                giocoTombola(true, numeroGiocatori);
 
                 break;
             }
@@ -91,7 +90,220 @@ int main() {
                 printf("\n\nInserire numero giocatori: ");
                 scanf("%d", &numeroGiocatori);
 
-                giocoTombola(pausaTurni, numeroGiocatori);
+
+
+                ////////////////////////////////////
+                // Gioco tombola con statistiche.
+                ////////////////////////////////////
+
+                int nTurniAmbo, nTurniTerna, nTurniQuaterna, nTurniCinquina;
+                bool ambo = false, terna = false, quaterna = false, cinquina = false, vittoria = false;
+
+                // Struttura con tutte le cartelle dei giocatori.
+                cartella cartelle[numeroGiocatori];
+
+                // Generazione cartelle.
+                generatoreCartelle(numeroGiocatori, cartelle, false);
+
+                // Inizio del gioco.
+                printf("\n\nLe cartelle sono state generate, ecco un riepilogo: ");
+
+                for (int i = 0; i < numeroGiocatori; i++) {
+                    printf("\n\n-------------------------------------------------------------------"
+                           "\n| Cartella giocatore %d: "
+                           "\n-------------------------------------------------------------------\n", i + 1);
+                    mostraCartella(cartelle, i);
+                    printf("-------------------------------------------------------------------");
+                }
+
+                if (pausaTurni) {
+                    continua();
+                }
+
+                printf("\n\n-------------------------------------------------------------------"
+                       "\n| Inizio del gioco, inserire un carattere per continuare ad ogni turno!"
+                       "\n-------------------------------------------------------------------");
+
+                // Gioco effettivo.
+                bool numeriGenerati[90] = {false};
+                int numeroRound = 0;
+                bool tombola = false;
+                while (!vittoria && numeroRound < 90){
+                    numeroRound++;
+
+                    // Cerca un numero che non sia gia' stato estratto.
+                    int numeroGenerato = randomMaxMin(1, 89);
+                    while (numeriGenerati[numeroGenerato]){
+                        numeroGenerato = randomMaxMin(1, 89);
+                    }
+
+                    // Aggiunge il numero a quelli estratti in modo che non si ripeta.
+                    numeriGenerati[numeroGenerato] = true;
+
+                    printf("\n\n-------------------------------------------------------------------"
+                           "\n| Numero estratto: %d"
+                           "\n-------------------------------------------------------------------", numeroGenerato);
+
+                    // Controlla cartelle.
+                    bool tipoEventi[5] = {false}; // 0 = Ambo, 1 = Terna, 2 = Quaterna, 3 = Cinquina, 4 = Tombola.
+                    int quantiEventi = 0;
+                    string azioneNome;
+                    string prossimaAzioneNome;
+                    for (int i = 0; i < numeroGiocatori; i++) {
+
+                        bool trovatoNumero, tombolaRichiesta = false;
+                        int veriRichiesti;
+                        // Controlla singolo giocatore, partendo dalle righe.
+                        trovatoNumero = controlloNumeroCG(cartelle, numeroGenerato, i);
+
+
+                        // Controllare se qualcuno ha fatto qualcosa di valido sulla sua cartella.
+                        if (!ambo){
+                            veriRichiesti = 2;
+                            azioneNome = "Ambo";
+                            prossimaAzioneNome = "Terna";
+                            nTurniAmbo = numeroRound;
+                        } else if (!terna){
+                            veriRichiesti = 3;
+                            azioneNome = "Terna";
+                            prossimaAzioneNome = "Quaterna";
+                            nTurniTerna = numeroRound;
+                        } else if (!quaterna){
+                            veriRichiesti = 4;
+                            azioneNome = "Quaterna";
+                            prossimaAzioneNome = "Cinquina";
+                            nTurniQuaterna = numeroRound;
+                        } else if (!cinquina){
+                            veriRichiesti = 5;
+                            azioneNome = "Cinquina";
+                            prossimaAzioneNome = "Tombola";
+                            nTurniCinquina = numeroRound;
+                        } else {
+                            tombolaRichiesta = true;
+                            azioneNome = "Tombola";
+                            prossimaAzioneNome = "Fine partita!";
+                        }
+
+
+
+                        if (trovatoNumero){
+
+                            // Controllo gli eventi già avvenuti
+                            bool lettoScheda = false;
+                            condizioniProssimoEvento(ambo, terna, quaterna, cinquina, azioneNome, prossimaAzioneNome,
+                                                     veriRichiesti, tombolaRichiesta);
+
+                            // Verifico se è richiesta la tombola ossia una condizione speciale.
+                            if (!tombolaRichiesta){
+
+                                // Controllo riga e conto quanti numeri sono stati trovati in essa.
+                                for (int j = 0; j < 3; j++) {
+
+                                    // Controllo colonna.
+                                    int numeroVeriTrovatiRiga = ContaTrovatiPerRiga(cartelle, i, j);
+
+                                    // Se sono stati trovati valori veri quanti quelli richiesti per il prossimo evento, proseguo e mostro la
+                                    // Cartella con i valori afflitti dall'evento.
+                                    if (numeroVeriTrovatiRiga == veriRichiesti){
+
+                                        printf("\n\n***-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-***"
+                                               "\n| Complimenti giocatore N.%d..."
+                                               "\n| Hai fatto %s!", i + 1, azioneNome.c_str());
+                                        printf("\n| Ecco la cartella con i valori evidenziati nei {x} (per l'evento) "
+                                               "\n| e -x- per quelli trovati:");
+                                        tipoEventi[veriRichiesti-2] = true;
+                                        quantiEventi++;
+
+                                        // Leggo righe.
+                                        printf("\n-------------------------------------------------------------------\n");
+                                        leggiCartellaEventiNormali(cartelle, i, j);
+                                        printf("-------------------------------------------------------------------");
+                                        lettoScheda = true;
+
+                                    }
+                                }
+
+                                if (!lettoScheda){
+
+                                    printf("\n\n-------------------------------------------------------------------"
+                                           "\n| Giocatore N.%d..."
+                                           "\n| Hai trovato un numero, ecco un riepilogo della tua scheda con"
+                                           "\n| I numeri trovati circondati da -x-:", i + 1);
+                                    printf("\n-------------------------------------------------------------------\n");
+                                    leggiCartellaTrovati(cartelle, i);
+                                    printf("-------------------------------------------------------------------");
+
+                                }
+                            } else {
+
+                                int numeroVeriTrovatiRiga = 0;
+                                numeroVeriTrovatiRiga = contaNumeriTrovati(cartelle, i, numeroVeriTrovatiRiga);
+
+                                if (numeroVeriTrovatiRiga == 15){
+
+                                    printf("\n\n*******************************************************************"
+                                           "\n| Congratulazioni giocatore N.%d! Hai fatto tombola!"
+                                           "\n*******************************************************************", i + 1);
+                                    tipoEventi[4] = true;
+                                    quantiEventi++;
+
+                                    printf("\n-------------------------------------------------------------------\n");
+                                    leggiCartellaTrovati(cartelle, i);
+                                    printf("-------------------------------------------------------------------");
+
+                                } else {
+
+                                    printf("\n\n-------------------------------------------------------------------"
+                                           "\n| Giocatore N.%d..."
+                                           "\n| Hai trovato un numero, ecco un riepilogo della tua scheda con"
+                                           "\n| I numeri trovati circondati da -x-:", i + 1);
+                                    printf("\n-------------------------------------------------------------------\n");
+                                    leggiCartellaTrovati(cartelle, i);
+                                    printf("-------------------------------------------------------------------");
+                                }
+                            }
+                        }
+
+                    }
+
+                    if (quantiEventi != 0) {
+
+                        if (!tipoEventi[4]) {
+
+                            printf("\nSono state trovate %d %s! Ora si punta a %s!", quantiEventi, azioneNome.c_str(), prossimaAzioneNome.c_str());
+
+                            cambiaStatoProssimoEvento(tipoEventi, ambo, terna, quaterna, cinquina, tombola);
+                        } else {
+
+                            printf("\n\nE' stata fatta tombola! Fine partita!");
+                            vittoria = true;
+
+                        }
+                    } else {
+                        printf("\n\nNessuno ha fatto %s, per favore procedere!", azioneNome.c_str());
+
+                    }
+
+                    if (pausaTurni) {
+                        continua();
+                    }
+                }
+                if (numeroRound > 90){
+                    printf("\n\nNessuno ha vinto in qualche modo, tutti i numeri sono stati estratti!");
+                }
+
+
+                // Statistiche output per questa partita.
+                printf("\n\nStatistiche generali di questo round di tombola:"
+                       "\nNumero giocatori: %d"
+                       "\nTurni ad ambo: %d"
+                       "\nTurni a terna: %d"
+                       "\nTurni a quarterna: %d"
+                       "\nTurni a cinquina: %d"
+                       "\nTurni a tombola: %d", numeroGiocatori, nTurniAmbo, nTurniTerna, nTurniQuaterna, nTurniCinquina, numeroRound);
+
+                // Aggiungere opzione DEBUG (vedere o meno ad ogni turno le azioni) e salvataggio su FILE delle statistiche con
+                // Sottomenu per controllare i LOG delle partite e leggerli.
 
                 continua();
                 break;
@@ -120,7 +332,7 @@ void giocoTombola(bool pausaTurni, int numeroGiocatori) {
     cartella cartelle[numeroGiocatori];
 
     // Generazione cartelle.
-    generatoreCartelle(numeroGiocatori, cartelle);
+    generatoreCartelle(numeroGiocatori, cartelle, true);
 
     // Inizio del gioco.
     printf("\n\nLe cartelle sono state generate, ecco un riepilogo: ");
@@ -481,7 +693,7 @@ bool controlloNumeroCG(cartella *cartelle, int numeroGenerato, int i) {
     return trovatoNumero;
 }
 
-void generatoreCartelle(int numeroGiocatori, cartella *cartelle) {
+void generatoreCartelle(int numeroGiocatori, cartella *cartelle, bool puoScegliere){
     int generateSuccesso = 0;
     // Continua a generare fino a quando non l'ha fatto per tutti i giocatori.
     while (generateSuccesso < numeroGiocatori){
@@ -542,12 +754,16 @@ void generatoreCartelle(int numeroGiocatori, cartella *cartelle) {
             mostraCartella(cartelle, generateSuccesso);
             printf("-------------------------------------------------------------------");
 
-            printf("\n\nVuoi tenerla o crearne una nuova?"
-                   "\n0 -> Crea una nuova."
-                   "\n1 -> Tieni questa."
-                   "\nPS: Qualsiasi numero per crearne una nuova."
-                   "\nScelta: ");
-            scanf("%d", &conferma);
+            if (puoScegliere) {
+                printf("\n\nVuoi tenerla o crearne una nuova?"
+                       "\n0 -> Crea una nuova."
+                       "\n1 -> Tieni questa."
+                       "\nPS: Qualsiasi numero per crearne una nuova."
+                       "\nScelta: ");
+                scanf("%d", &conferma);
+            } else {
+                conferma = 1;
+            }
 
             if (conferma == 1) {
                 generateSuccesso++;
