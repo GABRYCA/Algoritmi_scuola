dab65a45af22196fcac264f2ba8ee59509f7c870
GABRYCA <gabrycaretti@gmail.com>
2021-06-08 13:09:07 +0200

    Scritto algoritmo numero casuale con condizioni particolari.

---
+++ C++/Compiti Vacanze/NumeroCasuale/cmake-build-debug/CMakeFiles/NumeroCasuale.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NumeroCasuale.dir/main.cpp.obj"
+  "NumeroCasuale.exe"
+  "NumeroCasuale.exe.manifest"
+  "NumeroCasuale.pdb"
+  "libNumeroCasuale.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NumeroCasuale.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ C++/Puntatore/cmake-build-debug/CMakeFiles/Puntatore.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Puntatore.dir/main.cpp.obj"
+  "Puntatore.exe"
+  "Puntatore.exe.manifest"
+  "Puntatore.pdb"
+  "libPuntatore.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Puntatore.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
