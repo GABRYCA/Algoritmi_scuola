1159c1c81b643f77a6e87719ff98dbefe3ba3507
GABRYCA <gabrycaretti@gmail.com>
2021-06-09 11:07:03 +0200

    Scritto algoritmo testo in "farfallino".

--- C++/Compiti Vacanze/Farfallino/cmake-build-debug/CMakeFiles/Farfallino.dir/DependInfo.cmake
+++ C++/Compiti Vacanze/Farfallino/cmake-build-debug/CMakeFiles/Farfallino.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Users/GABRYCA/CLionProjects/Farfallino/main.cpp" "C:/Users/GABRYCA/CLionProjects/Farfallino/cmake-build-debug/CMakeFiles/Farfallino.dir/main.cpp.obj"
+  "C:/Users/GABRYCA/CLionProjects/Algoritmi_scuola/C++/Compiti Vacanze/Farfallino/main.cpp" "C:/Users/GABRYCA/CLionProjects/Algoritmi_scuola/C++/Compiti Vacanze/Farfallino/cmake-build-debug/CMakeFiles/Farfallino.dir/main.cpp.obj"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
---
+++ C++/Compiti Vacanze/QuadratiPerfetti/cmake-build-debug/CMakeFiles/QuadratiPerfetti.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QuadratiPerfetti.dir/main.cpp.obj"
+  "QuadratiPerfetti.exe"
+  "QuadratiPerfetti.exe.manifest"
+  "QuadratiPerfetti.pdb"
+  "libQuadratiPerfetti.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/QuadratiPerfetti.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
