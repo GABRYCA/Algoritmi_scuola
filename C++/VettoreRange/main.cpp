452e64f72a10cabbc6ab6ce925100d99d85fc58b
AnonymousGCA <gabrycaretti@gmail.com>
2021-10-18 21:41:51 +0200

    Scritto meglio l'algoritmo secondo i criteri del prof, e nuovo algoritmo di partizione con seconda partizione (che credo porterà a fini ricorsivi nelle prossime lezioni).

--- C++/VettoreRange/main.cpp
+++ C++/VettoreRange/main.cpp
@@ -13,6 +13,8 @@ void genVetCasuali(int numeri, int max, int min, int *vettore);
 
 void mostraValori(int numeri, const int *vettore, bool mostraValori);
 
+int partition(int *A, int inizio, int nNumeri);
+
 bool debug = false;
 
 int main() {
@@ -27,6 +29,8 @@ int main() {
         printf("\n\nScelte:"
                "\n0 -> Esci."
                "\n1 -> Genera vettore di numeri casuali e sposta i numeri rispetto al valore del primo."
+               "\n2 -> Partition del professore."
+               "\n3 -> Partition delle partizioni gia' fatte."
                "\nScelta: ");
         scanf("%d", &scelta);
 
@@ -133,6 +137,95 @@ int main() {
                 break;
             }
 
+            case 2:{
+
+                printf("\n\nHai scelto: Partition migliorato e ispirato allo pseudo-codice del professore.");
+
+                int numeri, max, min;
+                printf("\nInserire il numero di numeri: ");
+                scanf("%d", &numeri);
+
+                printf("\nInserire il numero massimo: ");
+                scanf("%d", &max);
+
+                printf("\nInserire il numero minimo: ");
+                scanf("%d", &min);
+
+                printf("\n\nGenerando un vettore con %d numeri casuali tra %d e %d...", numeri, min, max);
+
+                int* vettore = new int[numeri];
+                genVetCasuali(numeri, max, min, vettore);
+
+                printf("\n\nVettore con numeri casuali generato con successo!");
+                printf("\n\nPartition in corso tramite valore %d...", vettore[0]);
+
+                clock_t inizioT = clock();
+
+                partition(vettore, 0, numeri);
+
+                clock_t fineT = clock();
+
+                printf("\n\nOperazione completata con successo!");
+
+                // Tempo necessario al riordinamento.
+                unsigned long tempoOrdinamento = (fineT - inizioT) / CLOCKS_PER_SEC;
+
+                printf("\nIl tempo impiegato per eseguire l'operazione con %d numeri e' stato di %lu secondi.", numeri, tempoOrdinamento);
+
+                mostraValori(numeri, vettore, true);
+
+                pausa();
+                break;
+            }
+
+            case 3:{
+                printf("\n\nHai scelto: Partition delle Partition gia' fatte (doppio partition).");
+
+                int numeri, max, min;
+                printf("\nInserire il numero di numeri: ");
+                scanf("%d", &numeri);
+
+                printf("\nInserire il numero massimo: ");
+                scanf("%d", &max);
+
+                printf("\nInserire il numero minimo: ");
+                scanf("%d", &min);
+
+                printf("\n\nGenerando un vettore con %d numeri casuali tra %d e %d...", numeri, min, max);
+
+                int* vettore = new int[numeri];
+                genVetCasuali(numeri, max, min, vettore);
+
+                printf("\n\nVettore con numeri casuali generato con successo!");
+                printf("\n\nPartition in corso tramite valore %d...", vettore[0]);
+
+                clock_t inizioT = clock();
+
+                int incontro = partition(vettore, 0, numeri);
+
+                printf("\nPrima partition completata, inizio seconda (da inizio a punto d'incontro), valore %d...", vettore[0]);
+
+                partition(vettore, 0, incontro);
+
+                printf("\nSeconda partition completata, inizio terza (da punto d'incontro a fine), valore %d...", vettore[incontro]);
+
+                partition(vettore, incontro, numeri);
+
+                clock_t fineT = clock();
+
+                printf("\n\nOperazione completata con successo!");
+
+                // Tempo necessario al riordinamento.
+                unsigned long tempoOrdinamento = (fineT - inizioT) / CLOCKS_PER_SEC;
+
+                printf("\nIl tempo impiegato per eseguire l'operazione con %d numeri e' stato di %lu secondi.", numeri, tempoOrdinamento);
+
+                mostraValori(numeri, vettore, true);
+
+                pausa();
+                break;
+            }
+
             default:{
 
                 printf("\nHai scelto un valore non valido, per favore riprovare!");
@@ -146,6 +239,43 @@ int main() {
     printf("\nUscito con successo!");
 
     return 0;
+}
+
+/**
+ * A e' il vettore.
+ * inizio e' la posizione iniziale o fin dove e' ordinato.
+ * nNumeri e' il numero di numeri, ossia la fine o fin dove ordinare.
+ *
+ * La condizione di fine resta nel fatto che quando questi due si incontrano (diventano uguali), allora si ha finito
+ * e si ritorna il valore della posizione in cui si incontrano.
+ * */
+int partition(int *A, int inizio, int nNumeri){
+
+    int val = A[inizio];
+    int inizioPos = inizio;
+    int fine = nNumeri - 1;
+    while (true) {
+
+        while (A[fine] >= val){
+            fine--;
+        }
+
+        if (inizioPos == inizio){
+            swap(A[inizioPos], A[fine]);
+            inizioPos++;
+            fine--;
+        }
+
+        while (A[inizioPos] <= val){
+            inizioPos++;
+        }
+
+        if (inizioPos < fine){
+            swap(A[inizioPos], A[fine]);
+        } else {
+            return fine;
+        }
+    }
 }
 
 void mostraValori(int numeri, const int *vettore, bool mostraValori) {
