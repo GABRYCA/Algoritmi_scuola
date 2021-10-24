7eb5805e6c34617c52bb791b4957fddf03873a1f
AnonymousGCA <gabrycaretti@gmail.com>
2021-10-24 18:43:15 +0200

    Dopo una cosa come 15 ore totali, sono riuscito a far funzionare il debug nel modo corretto, ora bisognerà sclerare per inserire la graphics e applicarla, il debug è fatto in un modo che sia possibile applicare la graphics, se la graphics non funzionasse, procedere con l'analisi e, se il professore non riesce a trovare un modo di aiutarci, usare codeblocks, e se non fosse permesso codeblocks, "insultare" il professore applicando soluzioni non permesso come grafiche strane con i caratteri.

--- C++/Hanoi/main.cpp
+++ C++/Hanoi/main.cpp
@@ -13,8 +13,19 @@ bool inputBool();
 
 void ordinaCilindri(int num, int A, int B, int C);
 
+void ordinaCilindriGrafico(int num, int A, int B, int C);
+
+void graficaVettoriAttuale();
+
+void resetVett();
+
+void setColonnaABase(int nCilindri);
+
+int posCilCimaVet(int nVet);
+
 bool pausaClick;
-int delay = 100;
+int delay = 100, nCilindriG = 0;
+int vet[100][3] = {0};
 
 int main() {
 
@@ -27,6 +38,7 @@ int main() {
         printf("\n\nEcco una lista delle scelte disponibili: "
                "\n0 -> Esci."
                "\n1 -> Hanoi."
+               "\n2 -> Hanoi grafico."
                "\nScelta: ");
         scanf("%d", &scelta);
 
@@ -40,7 +52,7 @@ int main() {
 
             case 1:{
 
-                printf("\n\nHai scelto: Hanoi...");
+                printf("\n\nHai scelto: Hanoi numerico...");
 
                 int nCilindri;
                 do {
@@ -67,6 +79,53 @@ int main() {
 
                 ordinaCilindri(nCilindri, 1, 2, 3);
 
+                printf("\nCompletato con successo.");
+                break;
+            }
+
+            case 2:{
+
+                printf("\n\nHai scelto: Hanoi grafico...");
+
+                int nCilindri;
+                do {
+
+                    printf("\n\nInserire il numero di cilindri: ");
+                    scanf("%d", &nCilindri);
+
+                    if (nCilindri == 0){
+                        printf("\nPer favore inserire un valore maggiore di 0.");
+                    }
+
+                } while (nCilindri <= 0);
+
+                nCilindriG = nCilindri;
+
+                // Resetta il vettore e setta la variabile globale del numero di cilindri a quella inserita dall'utente.
+                resetVett();
+
+                setColonnaABase(nCilindri);
+
+                printf("\nposCimaCil VET A %d", posCilCimaVet(0));
+                printf("\nposCimaCil VET B %d", posCilCimaVet(1));
+                printf("\nposCimaCil VET C %d", posCilCimaVet(2));
+
+                graficaVettoriAttuale();
+
+                printf("\nVuoi un delay oppure una pausa sul click (no per delay, si per pausa)? "
+                       "\nScelta: ");
+                pausaClick = inputBool();
+
+                if (!pausaClick){
+                    printf("\nInserire un delay in millisecondi (default 100ms): ");
+                    scanf("%d", &delay);
+                }
+
+                printf("\nNumero cilindri: %d.", nCilindri);
+
+                ordinaCilindriGrafico(nCilindri, 0, 1, 2);
+
+                printf("\nFine.");
                 break;
             }
 
@@ -85,6 +144,32 @@ int main() {
     return 0;
 }
 
+void ordinaCilindriGrafico(int num, int A, int B, int C){
+    if (num == 1){
+        // Sposta cilindro in cima.
+        vet[posCilCimaVet(A)][A] = 0;
+        vet[posCilCimaVet(B) + 1][B] = 1;
+        printf("\nSpostato disco 1 dalla colonna %d alla colonna %d.", A, B);
+        if (!pausaClick) {
+            Sleep(delay);
+        }
+        // Mostrare qui grafica.
+        graficaVettoriAttuale();
+        return;
+    }
+    ordinaCilindriGrafico(num - 1, A, C, B);
+    // Sposta cilindro in cima.
+    vet[posCilCimaVet(A)][A] = 0;
+    vet[posCilCimaVet(B) + 1][B] = num;
+    printf("\nSpostato disco %d dalla colonna %d alla colonna %d.", num, A, B);
+    if (!pausaClick) {
+        Sleep(delay);
+    }
+    // Mostrare qui grafica.
+    graficaVettoriAttuale();
+    ordinaCilindriGrafico(num - 1, C, B, A);
+}
+
 void ordinaCilindri(int num, int A, int B, int C){
     if (num == 1){
         printf("\n Spostato disco 1 dalla colonna %d alla colonna %d", A, B);
@@ -99,6 +184,45 @@ void ordinaCilindri(int num, int A, int B, int C){
         Sleep(100);
     }
     ordinaCilindri(num - 1, C, B, A);
+}
+
+int posCilCimaVet(int nVet){
+    int nVal = -1;
+    while (true){
+        if (vet[nVal + 1][nVet] != 0){
+            nVal++;
+        } else {
+            return nVal;
+        }
+    }
+}
+
+void graficaVettoriAttuale(){
+    for (int i = 0; i < 3; i++) {
+        printf("\n\nVet %d: ", i);
+        for (int j = 0; j < nCilindriG; j++) {
+            if (vet[j][i] != 0) {
+                printf("\n%d -> %d", j, vet[j][i]);
+            }
+        }
+    }
+
+    printf("\n");
+}
+
+void setColonnaABase(int nCilindri){
+    // Imposta i valori del vettore base per la grafica in modo ordinato.
+    for (int i = 0; i < nCilindri; i++) {
+        vet[i][0] = nCilindri - i;
+    }
+}
+
+void resetVett(){
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 100; j++) {
+            vet[j][i] = 0;
+        }
+    }
 }
 
 bool inputBool(){
