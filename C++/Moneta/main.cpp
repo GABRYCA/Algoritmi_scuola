89b81fbfaae20c2f673c1e5ed036a120adda6b33
AnonymousGCA <gabrycaretti@gmail.com>
2021-12-01 09:57:33 +0100

    Testa o croce.

--- C++/Moneta/main.cpp
+++ C++/Moneta/main.cpp
@@ -12,6 +12,7 @@ int main() {
         printf("\n\nOpzioni:"
                "\n0 -> Esci."
                "\n1 -> Lancia moneta."
+               "\n2 -> Gioco."
                "\nScelta: ");
         scanf("%d", &scelta);
 
@@ -30,6 +31,33 @@ int main() {
                 printf("\nRisultato lancio: %s", moneta.toString().c_str());
 
                 pausa();
+                break;
+            }
+
+            case 2:{
+                printf("\nHai scelto: Gioca...");
+
+                int sceltaGiocatore;
+                int lanciTesta = 0;
+                Moneta miaMoneta;
+                printf("\nGioco testa o croce - 5 lanci.");
+                printf("\nScegli testa (0) oppure croce (1): ");
+                scanf("%d", &sceltaGiocatore);
+                for (int i = 0; i < 5; i++) {
+                    miaMoneta.lancia();
+                    printf("%s\n", miaMoneta.toString().c_str());
+                    if (miaMoneta.isTesta()){
+                        lanciTesta++;
+                    }
+                }
+                if (sceltaGiocatore == 0 && lanciTesta > 2){
+                    printf(" -> Hai vinto ! \n");
+                } else {
+                    printf(" -> Ha vinto il PC! \n");
+                }
+
+                pausa();
+                break;
             }
 
             default:{
