8db26b5214b4ef3468733b9a7b66891d03a1de34
AnonymousGCA <gabrycaretti@gmail.com>
2021-12-01 09:53:08 +0100

    Gioco moneta.

--- C++/Moneta/main.cpp
+++ C++/Moneta/main.cpp
@@ -2,11 +2,50 @@
 #include <stdio.h>
 #include "Moneta.h"
 
+void pausa();
+
 int main() {
 
-    Moneta moneta;
+    int scelta;
+    do {
 
-    printf("%s", moneta.toString().c_str());
+        printf("\n\nOpzioni:"
+               "\n0 -> Esci."
+               "\n1 -> Lancia moneta."
+               "\nScelta: ");
+        scanf("%d", &scelta);
+
+        switch (scelta) {
+
+            case 0:{
+                printf("\nHai scelto: Esci...");
+                break;
+            }
+
+            case 1:{
+                printf("\nHai scelto: Lancia moneta...");
+
+                Moneta moneta;
+
+                printf("\nRisultato lancio: %s", moneta.toString().c_str());
+
+                pausa();
+            }
+
+            default:{
+                printf("\nHai inserito un valore non valido, per favore riprovare.");
+                break;
+            }
+        }
+
+    } while (scelta != 0);
+
+    printf("\nUscito con successo!");
 
     return 0;
+}
+
+void pausa(){
+    printf("\n\nInserire un numero per continuare: ");
+    scanf("%d");
 }
--- Java/Pag386/src/me/gca/Main.java
+++ Java/Pag386/src/me/gca/Main.java
@@ -3,7 +3,78 @@ package me.gca;
 public class Main {
 
     public static void main(String[] args) {
-        Moneta moneta = new Moneta();
-        System.out.println(moneta);
+
+        int scelta = 0;
+        do {
+            Util.printf("\nScelte: " +
+                    "\n0 -> Esci." +
+                    "\n1 -> Lancia moneta." +
+                    "\n2 -> Gioca." +
+                    "\nScelta: ");
+            scelta = Util.getScanner().nextInt();
+
+            switch (scelta){
+
+                case 0: {
+
+                    Util.printfn("\nHai scelto: Esci...");
+
+                    break;
+                }
+
+                case 1: {
+
+                    Util.printfn("\nHai scelto: Lancia moneta...");
+
+                    Moneta moneta = new Moneta();
+                    System.out.println(moneta);
+
+                    Util.continua();
+                    break;
+                }
+
+                case 2:{
+
+                    Util.printfn("\nHai scelto: Gioca...");
+
+                    int sceltaGiocatore = 0;
+                    int lanciTesta = 0;
+                    Util.printfn("\nGioco del testa o croce - 5 lanci.");
+                    Util.printf("Scegli testa (0) oppure croce (1):");
+                    sceltaGiocatore = Util.getScanner().nextInt();
+
+                    Moneta moneta = new Moneta();
+                    for (int i = 0; i < 5; i++){
+                        moneta.lancia();
+                        System.out.println(moneta.toString());
+                        if (moneta.isTesta()){
+                            lanciTesta++;
+                        }
+                    }
+                    if (sceltaGiocatore == 0 && lanciTesta > 2){
+                        Util.printfn(" -> Hai vinto !");
+                    } else {
+                        Util.printfn(" -> Ha vinto il PC!");
+                    }
+
+                    Util.continua();
+                    break;
+                }
+
+                default:{
+
+                    Util.printfn("Scelta non valida, per favore riprovare.");
+
+                    Util.continua();
+                    break;
+                }
+
+            }
+
+        } while (scelta != 0);
+
+        Util.printf("Uscito con successo!");
+
+
     }
 }
