03a799a1fb00da72ad202ed1f3a59c6e036b4741
AnonymousGCA <gabrycaretti@gmail.com>
2021-12-12 12:38:51 +0100

    Header e footer tramite javascript comune a tutte le pagine.

---
+++ HTML/Bootstrap/Esercizio_2-Chiaro/js/footer.js
@@ -0,0 +1,21 @@
+document.getElementById("footer").innerHTML = "<div class='container-fluid bg-success bg-opacity-50' id='footer'>" +
+    "<hr>" +
+    "<div class='container'>" +
+    "<div class='row text-start'>" +
+    "<div class='col'>" +
+    "<p class='h5 pb-1'>Indirizzo:</p>" +
+    "<p class='h6'>Via San Michele 1</p>" +
+    "<p class='h6'>28863 VB</p>" +
+    "<p class='h6'>Italia</p>" +
+    "</div>" +
+    "<div class='col'>" +
+    "<p class='h5 pb-1'>Contatti:</p>" +
+    "<p class='h6'>Tel: +39 3936662327</p>" +
+    "</div>" +
+    "<div class='col'>" +
+    "<p class='h5 pb-1'>Partita IVA:</p>" +
+    "<p class='h6'>IT 73246324600</p>" +
+    "</div>" +
+    "</div>" +
+    "</div>" +
+    "</div>"
