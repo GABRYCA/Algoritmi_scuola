a689fca1ba83981d601a4030d9917fdeb34a9065
AnonymousGCA <gabrycaretti@gmail.com>
2021-12-15 16:01:18 +0100

    Modifiche al footer e a tutte le pagine per funzionare correttamente.

--- HTML/Bootstrap/Esercizio_2-Chiaro/js/footer.js
+++ HTML/Bootstrap/Esercizio_2-Chiaro/js/footer.js
@@ -11,10 +11,17 @@ document.getElementById("footer").innerHTML = "<div class='container-fluid bg-su
     "<div class='col'>" +
     "<p class='h5 pb-1'>Contatti:</p>" +
     "<p class='h6'>Tel: +39 3936662327</p>" +
+    "<div class='row w-50 pt-1'>" +
+    "<div class='col'><a href='https://facebook.com' target='_blank'><i class='fab fa-facebook social'></i></a></div>" +
+    "<div class='col'><a href='https://twitter.com' target='_blank'><i class='fab fa-twitter social'></i></a></div>" +
+    "<div class='col'><a href='https://instagram.com' target='_blank'><i class='fab fa-instagram social'></i></a></div>" +
+    "</div>" +
     "</div>" +
     "<div class='col'>" +
-    "<p class='h5 pb-1'>Partita IVA:</p>" +
+    "<p class='h5'>Partita IVA:</p>" +
     "<p class='h6'>IT 73246324600</p>" +
+    "<p class='h5'>PEC:</p>" +
+    "<p class='h6'>pec@formazzatipica.it</p>" +
     "</div>" +
     "</div>" +
     "</div>" +
