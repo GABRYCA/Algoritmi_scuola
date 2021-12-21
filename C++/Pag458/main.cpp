445c95978a5ab35bd1fbf4be40a35518a11403b4
AnonymousGCA <gabrycaretti@gmail.com>
2021-12-21 10:23:34 +0100

    Changelogs: - Pag 458.

---
+++ C++/Pag458/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+void insInVet(int nNumeri, int *vet);
+
+using namespace std;
+
+int main() {
+
+    printf("/////////////////////////////////////////"
+            "\n   Gerarchia Cellulare di G.C. 4BITI"
+            "\n/////////////////////////////////////////\n");
+
+    int nNumeri;
+
+    printf("\nInserire il numero di numeri da memoriazzare: ");
+    scanf("%d", &nNumeri);
+
+    int *vet = new int(nNumeri);
+
+    printf("\nInserire %d numeri:\n");
+    insInVet(nNumeri, vet);
+
+    printf("\nI valori inseriti sono:\n");
+    for (int i = 0; i < nNumeri; i++) {
+        printf("%d: %d", i + 1, vet[i]);
+    }
+
+    return 0;
+}
+
+void insInVet(int nNumeri, int *vet) {
+    for (int i = 0; i < nNumeri; i++) {
+        printf("\n%d: ", i + 1);
+        cin >> vet[i];
+    }
+}
