ef4f95aae512ef5d7107b0ef59e23888abf1637c
AnonymousGCA <gabrycaretti@gmail.com>
2022-01-11 00:04:24 +0100

    Visualizzazione codice.

--- Java/Albero_Binario/src/me/gca/Main.java
+++ Java/Albero_Binario/src/me/gca/Main.java
@@ -6,7 +6,14 @@ public class Main {
 
         int[] numeri = {10,8,-9,0,15,0,3};
 
+        AlberoBin alberoBin = new AlberoBin();
 
+        alberoBin.crea_da_array(numeri, 0, numeri.length);
 
+        alberoBin.preorder1();
+        alberoBin.postorder1();
+        alberoBin.inorder1();
+
+        System.out.print("\n\n" + alberoBin.toString());
     }
 }
