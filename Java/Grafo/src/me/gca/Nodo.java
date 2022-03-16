95f26713ba77a8edc801ed2af50739ff1bc2822b
AnonymousGCA <gabrycaretti@gmail.com>
2022-03-16 09:21:11 +0100

    Commenti.

--- Java/Grafo/src/me/gca/Nodo.java
+++ Java/Grafo/src/me/gca/Nodo.java
@@ -8,40 +8,86 @@ public class Nodo implements Serializable {
     private int difficolta;
     private int altitudine;
 
+    /**
+     * Costruttore
+     */
     public Nodo() {
     }
 
+    /**
+     * Costruttore nodo.
+     *
+     * @param nome
+     * @param difficolta
+     */
     public Nodo(String nome, int difficolta) {
         this.nome = nome;
         this.difficolta = difficolta;
     }
 
+    /**
+     * Costruttore nodo.
+     *
+     * @param nome
+     * @param difficolta
+     * @param altitudine
+     */
     public Nodo(String nome, int difficolta, int altitudine) {
         this.nome = nome;
         this.difficolta = difficolta;
         this.altitudine = altitudine;
     }
 
+    /**
+     * Ritorna il nome del nodo.
+     *
+     * @return
+     */
     public String getNome() {
         return nome;
     }
 
+    /**
+     * Ritorna il numero della difficolta' del nodo.
+     *
+     * @return
+     */
     public int getDifficolta() {
         return difficolta;
     }
 
+    /**
+     * Ritorna l'altitudine del nodo.
+     *
+     * @return
+     */
     public int getAltitudine() {
         return altitudine;
     }
 
+    /**
+     * Setta il nome del nodo con quello specificato.
+     *
+     * @param nome
+     */
     public void setNome(String nome) {
         this.nome = nome;
     }
 
+    /**
+     * Setta la difficolta' con quella specificata.
+     *
+     * @param difficolta
+     */
     public void setDifficolta(int difficolta) {
         this.difficolta = difficolta;
     }
 
+    /**
+     * Setta l'altitudine con quella specificata.
+     *
+     * @param altitudine
+     */
     public void setAltitudine(int altitudine) {
         this.altitudine = altitudine;
     }
--- Java/Grafo/src/me/gca/NodoLista.java
+++ Java/Grafo/src/me/gca/NodoLista.java
@@ -11,44 +11,96 @@ public class NodoLista implements Serializable {
     private int altitudine;
     private List<Integer> adiacenze = new ArrayList<>();
 
+    /**
+     * Costruttore.
+     */
     public NodoLista() {
     }
 
+    /**
+     * Costruttore.
+     *
+     * @param nome
+     * @param difficolta
+     */
     public NodoLista(String nome, int difficolta) {
         this.nome = nome;
         this.difficolta = difficolta;
     }
 
+    /**
+     * Costruttore.
+     *
+     * @param nome
+     * @param difficolta
+     * @param altitudine
+     */
     public NodoLista(String nome, int difficolta, int altitudine) {
         this.nome = nome;
         this.difficolta = difficolta;
         this.altitudine = altitudine;
     }
 
+    /**
+     * Ritorna il nome del nodo.
+     *
+     * @return
+     */
     public String getNome() {
         return nome;
     }
 
+    /**
+     * Ritorna la difficolta' del nodo.
+     *
+     * @return
+     */
     public int getDifficolta() {
         return difficolta;
     }
 
+    /**
+     * Ritorna l'altitudine del nodo.
+     *
+     * @return
+     */
     public int getAltitudine() {
         return altitudine;
     }
 
+    /**
+     * Setta il nome del nodo.
+     *
+     * @param nome
+     */
     public void setNome(String nome) {
         this.nome = nome;
     }
 
+    /**
+     * Setta la difficolta' del nodo.
+     *
+     * @param difficolta
+     */
     public void setDifficolta(int difficolta) {
         this.difficolta = difficolta;
     }
 
+    /**
+     * Setta l'altitudine del nodo.
+     *
+     * @param altitudine
+     */
     public void setAltitudine(int altitudine) {
         this.altitudine = altitudine;
     }
 
+    /**
+     * Aggiungi adiacenza alla lista del nodo.
+     *
+     * @param nNodo
+     * @return
+     */
     public boolean addAdiacenza(int nNodo) {
         if (adiacenze.contains(nNodo)) {
             return false;
@@ -58,6 +110,12 @@ public class NodoLista implements Serializable {
         return true;
     }
 
+    /**
+     * Rimuovi adiacenza alla lista del nodo.
+     *
+     * @param nNodo
+     * @return
+     */
     public boolean removeAdiacenza(int nNodo) {
         if (!adiacenze.contains(nNodo)) {
             return false;
@@ -67,10 +125,20 @@ public class NodoLista implements Serializable {
         return true;
     }
 
+    /**
+     * Ritorna lista delle adiacenze.
+     *
+     * @return
+     */
     public List<Integer> getAdiacenze() {
         return adiacenze;
     }
 
+    /**
+     * Setta adiacenze da lista.
+     *
+     * @param adiacenze
+     */
     public void setAdiacenze(List<Integer> adiacenze) {
         this.adiacenze = adiacenze;
     }
