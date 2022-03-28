9c48dda7d73e686e443fde916c89a32e7083cd28
AnonymousGCA <gabrycaretti@gmail.com>
2022-03-28 12:10:48 +0200

    - Scanner di reti con form.

---
+++ C#/VisualStudio/IpScanner/IpScanner/IpScanner/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace IpScanner
+{
+    partial class PulsanteScansiona
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listConnessi = new System.Windows.Forms.ListBox();
+            this.listDisconnessi = new System.Windows.Forms.ListBox();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // listConnessi
+            // 
+            this.listConnessi.FormattingEnabled = true;
+            this.listConnessi.ItemHeight = 16;
+            this.listConnessi.Location = new System.Drawing.Point(12, 114);
+            this.listConnessi.Name = "listConnessi";
+            this.listConnessi.Size = new System.Drawing.Size(371, 324);
+            this.listConnessi.TabIndex = 0;
+            this.listConnessi.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listDisconnessi
+            // 
+            this.listDisconnessi.FormattingEnabled = true;
+            this.listDisconnessi.ItemHeight = 16;
+            this.listDisconnessi.Location = new System.Drawing.Point(407, 114);
+            this.listDisconnessi.Name = "listDisconnessi";
+            this.listDisconnessi.Size = new System.Drawing.Size(381, 324);
+            this.listDisconnessi.TabIndex = 1;
+            this.listDisconnessi.SelectedIndexChanged += new System.EventHandler(this.listDisconnessi_SelectedIndexChanged);
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(137, 33);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(489, 22);
+            this.txtIP.TabIndex = 2;
+            this.txtIP.Tag = "";
+            this.txtIP.Text = "172.22.166.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Inserire indirizzo:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(645, 33);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(143, 26);
+            this.btnStart.TabIndex = 5;
+            this.btnStart.Text = "Scansiona!";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 65);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(776, 23);
+            this.progressBar1.TabIndex = 6;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // PulsanteScansiona
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.listDisconnessi);
+            this.Controls.Add(this.listConnessi);
+            this.Name = "PulsanteScansiona";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listConnessi;
+        private System.Windows.Forms.ListBox listDisconnessi;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ C#/VisualStudio/IpScanner/IpScanner/IpScanner/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IpScanner
+{
+    public partial class PulsanteScansiona : Form
+    {
+        public PulsanteScansiona()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            string mioIP = txtIP.Text;
+
+            if (mioIP == "")
+            {
+                MessageBox.Show("Inserisci un IP valido");
+            }
+            else
+            {
+                Task.Factory.StartNew(() =>
+                {
+                    MessageBox.Show("Scansione avviata!");
+
+                });
+                PingSubnet(mioIP);
+            }
+        }
+
+        public void PingSubnet(string address)
+        {
+            Ping ping = new Ping(); // Creo pacchetto.
+            PingOptions pingOptions = new PingOptions(); // Come il pacchetto deve essere inizializzato.
+            pingOptions.DontFragment = true; // Non frammentare il pacchetto.
+            pingOptions.Ttl = 128; // Default
+            string data = "";
+            for (int i = 0; i < 32; i++)
+            {
+                data += "A";
+            }
+
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+            int timeout = 120; // Timeout in secondi.
+            progressBar1.Value = 0;
+            progressBar1.Maximum = 255;
+
+            for (int i = 0; i < 255; i++)
+            {
+                string host = address + i;
+                PingReply reply = ping.Send(host, timeout, buffer, pingOptions); // Invio il ping.
+                if (reply == null)
+                {
+                    Console.WriteLine("Ping fallito!");
+                }
+                else
+                {
+                    if (reply.Status == IPStatus.Success)
+                    {
+                        listConnessi.Items.Add(host);
+                    }
+                    else
+                    {
+                        listDisconnessi.Items.Add(host);
+                    }
+                    progressBar1.Value = i;
+                    this.Refresh();
+                }
+            }
+            MessageBox.Show("Scansione terminata");
+        }
+
+        private void listDisconnessi_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
