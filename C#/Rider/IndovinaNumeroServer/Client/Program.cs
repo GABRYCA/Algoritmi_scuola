eb6977edad754ccdaaee3245770357b92f0a06d3
AnonymousGCA <gabrycaretti@gmail.com>
2022-05-09 09:14:44 +0200

    Changelogs: - Indovina Numero con Socket, lato Server e Client.

---
+++ C#/Rider/IndovinaNumeroServer/Client/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    internal class Program
+    {
+        private static Socket socket;
+
+        public static void Main(string[] args)
+        {
+            var server = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1313);
+
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            var testoInserito = "";
+            var tentativi = 0;
+            do
+            {
+                try
+                {
+                    // Mi connetto al server.
+                    socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    socket.Connect(server);
+
+                    var nonValidoNumero = false;
+                    do
+                    {
+                        Console.WriteLine("Tentativo n." + (tentativi + 1));
+                        Console.Write("Inserire il numero per provare a indovinare: ");
+                        testoInserito = Console.ReadLine();
+                        tentativi++;
+
+                        if (testoInserito == "Stop") break;
+
+
+                        try
+                        {
+                            if (testoInserito != null) int.Parse(testoInserito);
+                        }
+                        catch (FormatException ignored)
+                        {
+                            Console.WriteLine("Il numero inserito non e' valido!");
+                            nonValidoNumero = true;
+                        }
+                    } while (nonValidoNumero);
+
+                    if (testoInserito == "Stop") break;
+
+                    socket.Send(Encoding.ASCII.GetBytes(testoInserito));
+                    Console.WriteLine("Numero inviato!\n");
+
+                    // Ricevo stringa da socket a cui ho inviato.
+                    var bytes = new byte[1024];
+                    var bytesRec = socket.Receive(bytes);
+                    var testoRicevuto = Encoding.ASCII.GetString(bytes, 0, bytesRec);
+
+                    Console.WriteLine(testoRicevuto + "\n");
+
+                    if (testoRicevuto == "Hai indovinato il numero!")
+                    {
+                        Console.WriteLine("Gioco finito, numero tentativi: " + tentativi);
+                        break;
+                    }
+
+                    socket.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Errore durante la connessione: " + e.Message);
+                    socket.Close();
+                    break;
+                }
+            } while (testoInserito != "Stop");
+
+            socket.Close();
+            Console.WriteLine("Fine gioco!");
+        }
+    }
+}
---
+++ C#/Rider/IndovinaNumeroServer/Server/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    internal class Program
+    {
+        private static Socket socket;
+        public static void Main(string[] args)
+        {
+            string numeroStringa = "";
+            Socket ricevuto;
+            
+            Console.WriteLine("Genero un numero casuale tra 0 e 100...");
+            // Genero numero casuale tra 0 e 100 da indovinare.
+            Random random = new Random();
+            int numero = random.Next(0, 100);
+            Console.WriteLine("Numero casuale generato: " + numero);
+            
+            // Creo il socket.
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(new IPEndPoint(IPAddress.Any, 1313));
+            
+            Console.WriteLine("Server avviato...");
+            do
+            {
+                socket.Listen(100);
+                ricevuto = socket.Accept();
+                byte[] buffer = new byte[ricevuto.SendBufferSize];
+                int letti = ricevuto.Receive(buffer);
+                byte[] formatted = new byte[letti];
+                for (int i = 0; i < letti; i++)
+                {
+                    formatted[i] = buffer[i];
+                }
+                numeroStringa = Encoding.ASCII.GetString(formatted);
+                Console.WriteLine("Numero ricevuto: " + numeroStringa);
+                
+                int numeroRicevuto = int.Parse(numeroStringa);
+                
+                if (numeroRicevuto == numero)
+                {
+                    Console.WriteLine("Hai indovinato il numero!");
+                    ricevuto.Send(Encoding.ASCII.GetBytes("Hai indovinato il numero!"));
+                    break;
+                }
+                
+                if (numeroRicevuto > numero)
+                {
+                    Console.WriteLine("Il numero che hai inserito e' maggiore di quello da indovinare!");
+                    ricevuto.Send(Encoding.ASCII.GetBytes("Il numero che hai inserito e' maggiore di quello da indovinare!"));
+                }
+                else
+                {
+                    Console.WriteLine("Il numero che hai inserito e' minore di quello da indovinare!");
+                    ricevuto.Send(Encoding.ASCII.GetBytes("Il numero che hai inserito e' minore di quello da indovinare!"));
+                }
+                
+            } while (numeroStringa != "stop");
+            socket.Close();
+            ricevuto.Close();
+            Console.WriteLine("Fine del gioco!");
+        }
+    }
+}
