4f096bd969ccd0de588c20fa7da8a4a196cad91f
AnonymousGCA <gabrycaretti@gmail.com>
2022-05-12 12:08:48 +0200

    Programma client-server traduttore.

---
+++ C#/Rider/DizionarioTraduttore/Client/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace DizionarioTraduttore
+{
+    internal class Program
+    {
+
+        private static Socket socket;
+        public static void Main(string[] args)
+        {
+            // Creo l'IPEndPoint.
+            var server = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1313);
+
+            // Creo il socket.
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            
+            string testoInserito;
+            do
+            {
+                try
+                {
+                    // Mi connetto al server.
+                    socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    socket.Connect(server);
+
+                    // Chiedo testo da tradurre all'utente.
+                    Console.Write("\nInserire testo da tradurre (o 'FINE OPERAZIONI' per chiudere): ");
+                    testoInserito = Console.ReadLine();
+
+                    // Invio il testo al server.
+                    socket.Send(Encoding.ASCII.GetBytes(testoInserito));
+
+                    // Controllo se il testo inserito è quello di stop.
+                    if (testoInserito == "FINE OPERAZIONI") break;
+
+                    // Ricevo la risposta.
+                    var buffer = new byte[1024];
+                    var bytesLetti = socket.Receive(buffer);
+                    var risposta = Encoding.ASCII.GetString(buffer, 0, bytesLetti);
+
+                    // Controllo se ho ricevuto dal server un testo comunicante delle parole da tradurre.
+                    if (!risposta.Contains("Parole_Non_Trovate:"))
+                    {
+                        Console.WriteLine(risposta);
+                    }
+                    else
+                    {
+                        // Traduzione parole mancanti.
+                        Console.WriteLine("\nAlcune parole mancanti nella traduzione!" +
+                                          "\n\nInserire traduzione per le parole: ");
+                        string[] paroleDaTradurre = risposta.Split(' ');
+
+                        socket.Close(); // Chiudo socket.
+                        for (int i = 1; i < paroleDaTradurre.Length - 1; i++)
+                        {
+                            // Mi connetto al server.
+                            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                            socket.Connect(server);
+
+                            Console.Write("Inserire traduzione parola: " + paroleDaTradurre[i]
+                                                                         + "\nTraduzione: ");
+                            string traduzione = Console.ReadLine();
+                            Console.WriteLine("Inviato al server: " + paroleDaTradurre[i] + ";" + traduzione + "\n");
+                            socket.Send(Encoding.ASCII.GetBytes(paroleDaTradurre[i] + ";" + traduzione));
+                            socket.Close(); // Chiudo socket.
+                        }
+                    }
+                    socket.Close();
+                }
+                catch (SocketException e) // Errore di connessione.
+                {
+                    Console.WriteLine("Errore durante la connessione, forse il server è offline?");
+                    break;
+                }
+            } while (testoInserito != "FINE OPERAZIONI");
+            socket.Close(); // Chiudo socket se aperto.
+            Console.WriteLine("Fine programma!");
+        }
+    }
+}
---
+++ C#/Rider/DizionarioTraduttore/Server/Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    internal class Program
+    {
+        private static Socket socket;
+        public static void Main(string[] args)
+        {
+            // Variabili.
+            string pathParole = "parole.txt";
+            Socket ricevuto;
+            
+            Console.WriteLine("Inizializzazione server..." +
+                              "\nCaricando parole da file...");
+            
+            // Leggo file delle parole.
+            List<Parola> parole = new List<Parola>();
+            using (StreamReader sr = new StreamReader(pathParole))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    parole.Add(new Parola(line));
+                }
+            }
+            
+            // Comunico numero parole trovate e avvio server.
+            Console.WriteLine("\nCaricate " + parole.Count + " parole." +
+                              "\nAvviando il server...");
+            
+            // Creo il socket.
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(new IPEndPoint(IPAddress.Any, 1313));
+            
+            // Avvio il server.
+            Console.WriteLine("Server avviato...");
+            string testoRicevuto;
+            do
+            {
+                // Metto in listen e attendo ricezione testo.
+                socket.Listen(100);
+                ricevuto = socket.Accept();
+                byte[] buffer = new byte[ricevuto.SendBufferSize]; // Buffer di ricezione.
+                int letti = ricevuto.Receive(buffer); // Ricevo dati.
+                byte[] formatted = new byte[letti]; // Array di byte formattato.
+                for (int i = 0; i < letti; i++) // Leggo i Byte ricevuti.
+                {
+                    formatted[i] = buffer[i];
+                }
+                testoRicevuto = Encoding.ASCII.GetString(formatted); // Decodifico il testo ricevuto.
+                
+                // Controllo se il testo ricevuto è quello di stop del server.
+                if (testoRicevuto == "FINE OPERAZIONI") break;
+                
+                // Stampo il testo ricevuto dal client.
+                Console.WriteLine("\nRicevuto: " + testoRicevuto);
+                
+                if (testoRicevuto.Contains(";")) // Se ho ricevuto una traduzione
+                {
+                    parole.Add(new Parola(testoRicevuto)); // Aggiungo la parola alla lista.
+                    Console.WriteLine("\nParola aggiunta: " + testoRicevuto); // Stampo la parola aggiunta.
+                }
+                
+                // Controllo se il testoRicevuto contiene parole non presenti nella lista.
+                string[] paroleSingole = testoRicevuto.Split(' ');
+                List<string> paroleNonTrovate = new List<string>();
+                foreach (string parola in paroleSingole) // Controllo se le singole parole sono presenti nella lista di Parola.
+                {
+                    bool contenuto = false;
+                    foreach (Parola parolaInParole in parole) // Controllo se nelle parole sia presente quella da tradurre.
+                    {
+                        if (parolaInParole.italiano == parola) // Se c'è la parola in italiano, significa che è presente.
+                        {
+                            contenuto = true;
+                        }
+                    }
+                    if (!contenuto) // Se non è presente nella lista.
+                    {
+                        Console.WriteLine("\nParola non presente: " + parola); // Stampo in console la parola non trovata.
+                        if (!paroleNonTrovate.Contains(parola)) // Se non è già stata trovata prima in una lettura precedente.
+                        {
+                            paroleNonTrovate.Add(parola); // Aggiungo parola alla lista di parole mancanti.
+                        }
+                    }
+                }
+
+                if (paroleNonTrovate.Count > 0) // Se ci sono parole mancanti nella lista.
+                {
+                    string lineaParole = ""; // Stringa che conterrà le parole mancanti.
+                    foreach (string parola in paroleNonTrovate) // Costruisco la stringa delle parole mancanti.
+                    {
+                        lineaParole += parola + " ";
+                    }
+                    byte[] bufferParole = Encoding.ASCII.GetBytes("Parole_Non_Trovate: " + lineaParole);
+                    Console.WriteLine("Inviato al client: " + "Parole_Non_Trovate: " + lineaParole);
+                    ricevuto.Send(bufferParole); // Invio al client le parole mancanti.
+                }
+                else
+                {
+                    foreach (Parola parola in parole) // Controllo parole.
+                    {
+                        if (testoRicevuto.Contains(parola.italiano)) // Controllo se presente nelle parole della stringa.
+                        {
+                            testoRicevuto = testoRicevuto.Replace(parola.italiano, parola.inglese); // Sostituisco la parola trovata.
+                        }
+                    }
+
+                    // Invio risposta.
+                    byte[] risposta = Encoding.ASCII.GetBytes("Tradotto: " + testoRicevuto);
+                    ricevuto.Send(risposta);
+                }
+            } while (testoRicevuto != "FINE OPERAZIONE");
+
+            // Chiudo il socket e spengo il server.
+            socket.Close();
+            ricevuto.Close();
+            
+            Console.WriteLine("Salvataggio su file in corso...");
+            // Salvo su file lista parole.
+            StreamWriter file = new StreamWriter(pathParole);
+            foreach (Parola parola in parole)
+            {
+                file.WriteLine(parola.ToString());
+            }
+            file.Close();
+            Console.WriteLine("Fine salvataggio e chiuso server con successo!");
+        }
+    }
+
+    public class Parola
+    {
+        public string italiano;
+        public string inglese;
+        
+        public Parola(string italiano, string inglese)
+        {
+            this.italiano = italiano;
+            this.inglese = inglese;
+        }
+
+        public Parola(string insieme)
+        {
+            string[] parole = insieme.Split(';');
+            italiano = parole[0];
+            inglese = parole[1];
+        }
+
+        public override string ToString()
+        {
+            return italiano + ";" + inglese;
+        }
+    }
+}
