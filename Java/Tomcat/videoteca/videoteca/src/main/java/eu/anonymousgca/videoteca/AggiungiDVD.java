4b1051b00c1cb2fee730355577185f2463e2a253
AnonymousGCA <39743848+GABRYCA@users.noreply.github.com>
2023-01-26 14:12:29 +0100

    Aggiunto logout, aggiungi utente e DVD.

---
+++ Java/Tomcat/videoteca/videoteca/src/main/java/eu/anonymousgca/videoteca/AggiungiDVD.java
@@ -0,0 +1,18 @@
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "AggiungiDVD", value = "/AggiungiDVD")
+public class AggiungiDVD extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ Java/Tomcat/videoteca/videoteca/src/main/webapp/aggiungidvd.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: gabry
+  Date: 26/01/2023
+  Time: 13:26
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
