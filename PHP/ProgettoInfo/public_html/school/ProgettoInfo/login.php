d63fd433c8644dea3304be36de7a008d4bf3472b
AnonymousGCA <39743848+GABRYCA@users.noreply.github.com>
2023-04-13 22:40:15 +0200

    Modifiche al sito generali.

--- PHP/ProgettoInfo/public_html/school/ProgettoInfo/login.php
+++ PHP/ProgettoInfo/public_html/school/ProgettoInfo/login.php
@@ -5,7 +5,7 @@ session_start();
 // Check if user is already logged in, if yes then redirect him to profile page.
 if (isset($_SESSION['loggedin'])) {
     header('Location: profilo.php');
-    exit;
+    return;
 }
 
 $con = connessione();
@@ -13,17 +13,21 @@ $con = connessione();
 // Verifico se i dati sono stati inseriti.
 if (!isset($_POST['username'], $_POST['password'])) {
     // Errore, non tutti i campi sono stati compilati.
-    exit('Devi compilare tutti i campi necessari!');
+    echo('Devi compilare tutti i campi necessari!');
+    header("refresh:2;url=login.html");
+    return;
 }
 
 // Controllo se username e password sono vuoti.
 if (empty($_POST['username']) || empty($_POST['password'])) {
     // Errore, non tutti i campi sono stati compilati.
-    exit('Devi compilare tutti i campi necessari!');
+    echo('Devi compilare tutti i campi necessari!');
+    header("refresh:2;url=login.html");
+    return;
 }
 
 // Ottengo dati account con username specificato.
-if ($stmt = $con->prepare('SELECT id_utente, password, activation_code FROM utente WHERE username = ?')) {
+if ($stmt = $con->prepare('SELECT id_utente, password, activation_code, email_personale FROM utente WHERE username = ?')) {
     // Parametri.
     $stmt->bind_param('s', $_POST['username']);
     $stmt->execute();
@@ -31,7 +35,7 @@ if ($stmt = $con->prepare('SELECT id_utente, password, activation_code FROM uten
     $stmt->store_result();
 
     if ($stmt->num_rows > 0) {
-        $stmt->bind_result($id, $password, $activation_code);
+        $stmt->bind_result($id, $password, $activation_code, $email_personale);
         $stmt->fetch();
 
         // Account trovato, controllo password.
@@ -41,7 +45,9 @@ if ($stmt = $con->prepare('SELECT id_utente, password, activation_code FROM uten
             // Successo, controllo se l'account è attivato.
             if (!$activation_code == 'activated') {
                 // Account non attivato.
-                exit('Il tuo account non è stato attivato, per favore controlla le tue email (e anche lo spam)!');
+                echo('Il tuo account non è stato attivato, per favore controlla le tue email (e anche lo spam)!');
+                header("refresh:2;url=login.html");
+                return;
             }
 
             // Creo una nuova sessione.
@@ -49,6 +55,31 @@ if ($stmt = $con->prepare('SELECT id_utente, password, activation_code FROM uten
             $_SESSION['loggedin'] = TRUE; // Variabile per riconoscere se l'utente è loggato.
             $_SESSION['name'] = $_POST['username']; // Variabile per riconoscere il nome dell'utente.
             $_SESSION['id'] = $id; // Variabile per riconoscere l'id dell'utente nel database (utente.id_utente).
+
+            // Loggo l'ip dell'utente se non è già presente in ip_utente.
+            $ip = $_SERVER['REMOTE_ADDR'];
+            if (!filter_var($ip, FILTER_VALIDATE_IP)) exit("L'ip non è valido (Sei in IPv6?, wow)!");
+
+            $stmt = $con->prepare("SELECT * FROM ip_utente WHERE ip = ?");
+            $stmt->bind_param("s", $ip);
+            $stmt->execute();
+            $stmt->store_result();
+            if ($stmt->num_rows == 0) {
+                // Aggiunge all'utente con id $id l'ip $ip.
+                $stmt = $con->prepare("INSERT INTO ip_utente (id_utente, ip) VALUES (?, ?)");
+                $stmt->bind_param("is", $id, $ip);
+                $stmt->execute();
+
+                // Invia email comunicando che è stato eseguito l'accesso con un nuovo ip, nel caso in cui fallisca l'invio dell'email, non si blocca l'accesso e non mostra l'errore.
+                $to = $email_personale;
+                $subject = "Nuovo accesso";
+                // Comunico nuovo accesso e avviso di cambiare la password se non era lui.
+                $message = "Ciao " . $_SESSION['name'] . ",\r \r e' stato eseguito un nuovo accesso con il tuo account da un nuovo ip: " . $ip . "\r \r Se non sei stato tu, ti consigliamo di cambiare IMMEDIATAMENTE la password accedendo al tuo profilo e utilizzare 'Modifica Password' nella danger zone rossa.";
+                // Da noreply@anonymousgca.eu
+                $headers = "From: noreply@anonymousgca.eu";
+                // Suppress warning
+                @mail($to, $subject, $message, $headers);
+            }
 
             echo 'Ciao ' . $_SESSION['name'] . ', ti sei loggato con successo! Per favore attendi un paio di secondi...';
             // Rimando alla pagina principale dopo 2 secondi.
